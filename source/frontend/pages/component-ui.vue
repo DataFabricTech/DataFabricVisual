6a925b98cab282a1de7de58cc5a84a0c66318220
ohmy04180 <ohmy0418@mobigen.com>
2023-10-13 15:25:24 +0900

    [FAB2023-135] feat: Button 컴포넌트 추가

--- source/frontend/pages/component-ui.vue
+++ source/frontend/pages/component-ui.vue
@@ -1,29 +1,53 @@
 <template>
-<div class="layout-guide">
-  <table class="guide-table">
-    <caption>Component UI</caption>
-    <tbody>
-    <tr>
-      <th scope="row">Badge</th>
-      <td>
-        <baseBadge class="bg-purple">Badge</baseBadge>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">Button</th>
-      <td>
-        <baseButton>Button</baseButton>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">Modal</th>
-      <td>
-        <baseButton @click="onClickOpen">Modal Open</baseButton>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-</div>
+  <div class="layout-guide">
+    <table class="guide-table">
+      <caption>Component UI</caption>
+      <tbody>
+      <tr>
+        <th scope="row">Badge</th>
+        <td>
+          <baseBadge class="bg-purple">Badge</baseBadge>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Button</th>
+        <td>
+          <div class="h-group">
+            <baseButton class="button-primary button-lg">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-link">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-sm">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-negative button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-normal">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-secondary button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Modal</th>
+        <td>
+          <baseButton @click="onClickOpen">
+            <span class="button-text">Modal Open</span>
+          </baseButton>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
   <modal v-model="show" title="Modal Sample">
     <template v-slot:body>
       ~ Modal is Here! Modal is Here! ~
@@ -31,8 +55,8 @@
   </modal>
 </template>
 <script lang="ts" setup>
-import {ref} from "@vue/reactivity";
-import {useRouter} from "vue-router";
+import { ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
 
 const show = ref(false);
 const router = useRouter();
@@ -40,8 +64,9 @@ const router = useRouter();
 function onClickOpen() {
   show.value = !show.value;
 }
+
 function onClickMain() {
-  router.push("/main")
+  router.push("/main");
 }
 
 
