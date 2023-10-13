f5aa5ce06f207791b17ccc4c22ee9a4edb1a8978
jungseolhwa <dhqkfskrwl@naver.com>
2023-10-13 17:13:19 +0900

    [FAB2023-175]badge,input 추가, 레이아웃 스타일 추가

---
+++ source/frontend/components/base/textinput.vue
@@ -0,0 +1,14 @@
+<template>
+  <input class="text-input" :type="props.type" :value="props.value" :disabled="props.disabled" :placeholder="props.placeholder">
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  type: { type: String, default: "text" },
+  value: { type: String, default: null },
+  placeholder: { type: String, default: null },
+  disabled: { type: Boolean, default: false },
+});
+
+</script>
