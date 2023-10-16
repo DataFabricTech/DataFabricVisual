4759eadd0dbba463976df3c17654701523b0691f
ohmy04180 <ohmy0418@mobigen.com>
2023-10-16 14:08:52 +0900

    [FAB2023-198] feat: Component - Button UI 퍼블리싱

---
+++ source/frontend/pages/component-ui-omy.vue
@@ -0,0 +1,141 @@
+<template>
+<div class="layout-guide">
+  <div class="layout-guide-title">
+    <h1>Fabric UI Components</h1>
+  </div>
+  <table class="guide-table">
+    <tbody>
+    <tr>
+      <th scope="row">Button</th>
+    </tr>
+    <tr>
+      <td>
+        <div class="v-group gap-3">
+          <div class="h-group">
+            <baseButton title="사용자 관리" class="button-primary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-secondary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-tertiary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-negative">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-danger">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-normal">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-link button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-link-primary button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-link-negative button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-link-negative button-sm" disabled>
+              <svg-icon name="user" class="svg-icon" />
+              <span class="button-text">Disabled</span>
+            </baseButton>
+          </div>
+          <div class="h-group">
+            <baseButton class="button-primary button-lg">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-secondary button-lg">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-tertiary button-lg">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-negative">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-danger">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-normal">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-link button-sm">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-link-primary button-sm">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-link-negative button-sm">
+              <span class="button-text">Button</span>
+            </baseButton>
+            <baseButton class="button-primary button-lg" disabled>
+              <span class="button-text">Disabled</span>
+            </baseButton>
+          </div>
+          <div class="h-group">
+            <baseButton title="사용자 관리" class="button-icon button-primary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-secondary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-tertiary button-lg">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-negative">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-danger">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-normal">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-link button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-link-primary button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-link-negative button-sm">
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+            <baseButton title="사용자 관리" class="button-icon button-normal" disabled>
+              <svg-icon name="user" class="svg-icon" />
+              <span class="hidden">사용자 관리</span>
+            </baseButton>
+          </div>
+        </div>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+<script lang="ts" setup></script>
+
+<style lang="scss">
+@import "component-ui";
+</style>
