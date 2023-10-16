cd68f8f24be2e68107b66033116f7e31b4634258
jungseolhwa <dhqkfskrwl@naver.com>
2023-10-16 15:13:41 +0900

    [FAB2023-197]textinput, radio, tag컴포넌트 추가-중간저장

---
+++ source/frontend/components/base/radio.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="radio">
+    <input class="radio-input" type="radio" :id="props.id" :name="props.name" :disabled="props.disabled" :checked="props.checked">
+    <label :for="props.id" class="radio-label"><slot>label</slot></label>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  type: { type: String, default: "radio" },
+  id: { type: String, default: null},
+  name: { type: String, default: null},
+  disabled: { type: Boolean, default: false },
+  checked: { type: Boolean, default: false },
+});
+
+</script>
---
+++ source/frontend/components/base/tag.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="tag">
+    <span class="hashtag">#</span>
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
