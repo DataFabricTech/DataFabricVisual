f59180d70373f1ddbe2e5bbff4465b091ada6b3c
codemotion19 <codemotion@mobigen.com>
2023-10-17 13:19:07 +0900

    [FAB2023-175] Componet UI - 퍼블리싱

---
+++ source/frontend/components/base/step.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="step">
+    <div class="step-item">
+      <div class="step-icon">
+        <span class="step-icon-text">
+          1
+        </span>
+      </div>
+      <div class="step-title">
+        <span class="step-title-text">
+          <slot></slot>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
---
+++ source/frontend/pages/component-ui-kyj.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="layout-guide">
+  <div class="layout-guide-title">
+    <h1>Fabric UI Components</h1>
+  </div>
+  <table class="guide-table">
+    <tbody>
+    <tr>
+      <th scope="row">Step</th>
+    </tr>
+    <tr>
+      <td>
+        <div class="flex gap-8">
+          <baseStep>step</baseStep>
+          <baseStep class="step-current">step</baseStep>
+        </div>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+  <modal v-model="show" title="Modal Sample">
+    <template v-slot:body>
+      ~ Modal is Here! Modal is Here! ~
+    </template>
+  </modal>
+</template>
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+
+const show = ref(false);
+const router = useRouter();
+
+function onClickOpen() {
+  show.value = !show.value;
+}
+
+function onClickMain() {
+  router.push("/main");
+}
+
+
+</script>
+
+<style lang="scss">
+@import "component-ui";
+</style>
