6032dd3d2ca3ae9550632315c0687dbd41215d30
codemotion19 <codemotion@mobigen.com>
2023-10-17 15:09:14 +0900

    [FAB2023-201] feat: component - step 퍼블리싱

--- source/frontend/pages/component-ui-kyj.vue
+++ source/frontend/pages/component-ui-kyj.vue
@@ -11,8 +11,56 @@
     <tr>
       <td>
         <div class="flex gap-8">
-          <baseStep>step</baseStep>
-          <baseStep class="step-current">step</baseStep>
+          <div class="step">
+            <div class="step-item">
+              <div class="step-icon">
+                <span class="step-icon-text">
+                  1
+                </span>
+              </div>
+              <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+              </div>
+            </div>
+            <div class="step-item is-current">
+              <div class="step-icon">
+              <span class="step-icon-text">
+                2
+              </span>
+              </div>
+              <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+              </div>
+            </div>
+            <div class="step-item">
+              <div class="step-icon">
+                <span class="step-icon-text">
+                  3
+                </span>
+              </div>
+              <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+              </div>
+            </div>
+            <div class="step-item">
+              <div class="step-icon">
+                <span class="step-icon-text">
+                  4
+                </span>
+              </div>
+              <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </td>
     </tr>
