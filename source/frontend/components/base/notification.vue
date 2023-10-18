c409868c33a7e676003551e110795ac959c19522
ohmy04180 <ohmy0418@mobigen.com>
2023-10-18 13:29:17 +0900

    [FAB2023-198] feat: component - notification 퍼블리싱 1

---
+++ source/frontend/components/base/notification.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="notification">
+    <div class="notification-icon"></div>
+    <div class="notification-contents">
+      <strong class="notification-title">
+        <em>연결정보 H</em>의 연결정보가 변동되어 데이터를 불러올 수 없습니다.
+      </strong>
+      <a href="#" class="notification-link">
+        바로가기
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
