959929246d401ba583332e6f0c60a4c50b72a54f
ohmy04180 <ohmy0418@mobigen.com>
2023-10-19 13:58:25 +0900

    [FAB2023-198] fix: 코드 정리 및 컴포넌트 수정

--- source/frontend/components/base/context.vue
+++ source/frontend/components/base/context.vue
@@ -2,18 +2,31 @@
   <div class="context">
     <ul class="context-list">
       <li class="context-item">
-        <button class="context-button">
-          <slot name="menu-01"></slot>
+        <!--        현재 위치인 경우 is-selected 클래스 추가 -->
+        <button class="context-button is-selected" type="button">
+          <span class="context-text">선택된 상태</span>
         </button>
       </li>
-      <li class="context-item is-selected">
-        <button class="context-button">
-          <slot name="menu-02"></slot>
+      <li class="context-item">
+        <button class="context-button" type="button">
+          <span class="context-text">이름이 길어지면 말줄임</span>
         </button>
       </li>
-      <li class="context-item is-disabled">
-        <button class="context-button">
-          <slot name="menu-03"></slot>
+      <li class="context-item">
+        <button class="context-button" type="button">
+          <span class="context-text">우측 아이콘 유형</span>
+          <svg-icon name="string-ascending" class="svg-icon" />
+        </button>
+      </li>
+      <li class="context-item">
+        <button class="context-button" type="button">
+          <span class="context-text">우측 아이콘 유형</span>
+          <svg-icon name="string-descending" class="svg-icon" />
+        </button>
+      </li>
+      <li class="context-item">
+        <button class="context-button" type="button" disabled>
+          <span class="context-text">비활성화 상태</span>
         </button>
       </li>
     </ul>
--- source/frontend/components/base/step.vue
+++ source/frontend/components/base/step.vue
@@ -1,5 +1,54 @@
 <template>
-
+  <div class="step">
+    <div class="step-item">
+      <div class="step-icon">
+                <span class="step-icon-text">
+                  1
+                </span>
+      </div>
+      <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+      </div>
+    </div>
+    <div class="step-item is-current">
+      <div class="step-icon">
+              <span class="step-icon-text">
+                2
+              </span>
+      </div>
+      <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+      </div>
+    </div>
+    <div class="step-item">
+      <div class="step-icon">
+                <span class="step-icon-text">
+                  3
+                </span>
+      </div>
+      <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+      </div>
+    </div>
+    <div class="step-item">
+      <div class="step-icon">
+                <span class="step-icon-text">
+                  4
+                </span>
+      </div>
+      <div class="step-title">
+                <span class="step-title-text">
+                  step
+                </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
