f933889b4744b573ded317388b65105fb82115e5
sominJoo <js06m13@gmail.com>
2023-10-19 17:10:27 +0900

    프로젝트 폴더 구조 및 샘플 코드 추가

---
+++ source/frontend/components/common/alert.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>alert 컴포넌트</h1>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ source/frontend/pages/sample/component-jsm.vue
@@ -0,0 +1,13 @@
+<template>
+  <tab-bar></tab-bar>
+  <alert></alert>
+  <p>composables test : {{ sampleData }}</p>
+</template>
+
+<script lang="ts" setup>
+import { sample } from "../../composables/sample";
+import TabBar from "../../components/project/tab-bar.vue";
+import Alert from "../../components/common/alert.vue";
+
+const sampleData = sample();
+</script>
