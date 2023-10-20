fc4617aff2222a66944c6f380372d2044955e587
ohmy04180 <ohmy0418@mobigen.com>
2023-10-20 13:27:24 +0900

    [FAB2023-181] feat: 데이터패브릭 검색결과 화면 구성

---
+++ source/frontend/components/search-filter.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="search-filter">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
--- source/frontend/pages/data-fabric/search/index.vue
+++ source/frontend/pages/data-fabric/search/index.vue
@@ -1,23 +1,49 @@
 <!-- 데이터패브릭 - 검색 -->
 <template>
-  <div class="l-split">
-    <div class="l-inner gap-[20px]">
-      <h2 class="hidden">데이터패브릭 검색</h2>
-      <div class="snb">
-        <h3 class="hidden">필터</h3>
-        SNB
-      </div>
-      <div class="main-content">
-        <h3 class="hidden">검색 결과 목록</h3>
-        <!--      <section></section>-->
-        CONTENTS
-      </div>
+  <div class="l-common">
+    <div class="l-inner">
+      <h2 class="hidden">테이버패브릭 검색</h2>
+      <section>
+        <h3 class="hidden">데이터 검색</h3>
+        <div class="data-search">
+          <!--            TODO: [퍼블리싱] search-field 대체 예정-->
+          <BaseTextInput></BaseTextInput>
+          <BaseButton class="button-icon button-link">
+            <svg-icon name="filter" class="svg-icon"></svg-icon>
+          </BaseButton>
+        </div>
+      </section>
+      <section>
+        <h3 class="hidden">데이터 검색 결과</h3>
+        <BaseTab theme="round-tab"></BaseTab>
+        <div class="result-info">
+          <h4 class="result-summary">검색 결과는 <em>4</em> 건 입니다.</h4>
+          <div class="result-sort">
+            <div class="view-type">
+
+            </div>
+          </div>
+        </div>
+        <strong class="result-category">
+
+        </strong>
+        <ul class="result-list">
+          <li class="result-item">
+            <Card>
+              <template v-slot:img>
+                <svg-icon name="data" class="svg-icon"></svg-icon>
+              </template>
+            </Card>
+          </li>
+        </ul>
+        <!--      ...-->
+      </section>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 definePageMeta({
-  layout: "default-tab-layout"
+  layout: "default-layout"
 });
 
 </script>
