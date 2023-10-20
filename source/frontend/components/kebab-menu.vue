3155f53f7b96385dda9ad21de15ea136f8a56f21
ohmy04180 <ohmy0418@mobigen.com>
2023-10-20 09:14:11 +0900

    [FAB2023-181] feat: component - card, kebab-menu 퍼블리싱

---
+++ source/frontend/components/kebab-menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="kebab-menu">
+    <BaseButton @focus="state.isOpen=true" @blur="state.isOpen = false" class="button button-icon button-lg" title="열기" >
+      <span class="hidden">상세설정 열기</span>
+      <svg-icon name="kebab-menu" class="svg-icon" />
+    </BaseButton>
+    <BaseContext class="kebab-context" v-if="state.isOpen"></BaseContext>
+  </div>
+</template>
+
+<script setup lang="ts">
+const state = reactive({
+  isOpen: false
+});
+</script>
