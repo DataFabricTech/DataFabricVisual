d80260ea7d3144d950219e92eb1ac6dc68b2cd94
ohmy04180 <ohmy0418@mobigen.com>
2023-10-24 15:00:20 +0900

    [FAB2023-121] feat: modules - card-simple 컴포넌트 추가

---
+++ source/frontend/components/card-simple.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-simple">
+      <div class="h-group gap-[8px]">
+        <BaseBadge class="bg-marker-cyan">HDFS</BaseBadge>
+        <BaseBadge class="bg-marker-purple">공간</BaseBadge>
+        <BaseBadge class="bg-marker-red">MariaDB</BaseBadge>
+        <BaseBadge class="bg-marker-yellow">Table</BaseBadge>
+      </div>
+      <strong class="card-simple-title" title="이동">불법 주정차 구간 데이터</strong>
+      <p class="card-simple-detail">서울시에서 수집되고 있는 불법 주정차 차량 단속 이력 정보</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
