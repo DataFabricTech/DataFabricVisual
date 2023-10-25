c21bdf26e0dac9dca2e14bb3d31e983338d2ea4d
sominJoo <js06m13@gmail.com>
2023-10-25 18:03:15 +0900

    [isu-263] feat: 날짜 컴포넌트 개발

--- source/frontend/pages/sample/component-jsm.vue
+++ source/frontend/pages/sample/component-jsm.vue
@@ -1,8 +1,13 @@
 <template>
   <tab-bar></tab-bar>
   <alert></alert>
-  <date-picker></date-picker>
-  <highcharts style="width: 500px; height: 200px"  :options="chartOptions"> </highcharts>
+
+  <br>
+  <span> 단일 날짜 선택 : </span><date-picker v-model:modelValue="date"></date-picker>
+  <br>
+  <span> 기간 날짜 선택 : </span><date-picker v-model:modelValue="dateRange"></date-picker>
+  <br>
+  <highcharts style="width: 500px; height: 200px" :options="chartOptions"> </highcharts>
 
   <ag-grid-vue style="width: 500px; height: 200px" class="ag-theme-alpine" :columnDefs="columnDefs" :rowData="rowData">
   </ag-grid-vue>
@@ -13,7 +18,7 @@
 import { sample } from "../../composables/sample";
 import TabBar from "../../components/project/tab-bar.vue";
 import Alert from "../../components/common/alert.vue";
-import DatePicker from "../../components/common/date-picker.vue";
+import DatePicker from "../../components/common/date-picker/date-picker.vue";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -21,12 +26,15 @@ import { AgGridVue } from "ag-grid-vue3";
 
 const sampleData = sample();
 
+const date = ref("2023-11-22");
+const dateRange = ref(["2023-11-11", "2023-11-22"]);
+
 const chartOptions = {
   series: [
     {
-      data: [1, 2, 3], // sample data
-    },
-  ],
+      data: [1, 2, 3] // sample data
+    }
+  ]
 };
 
 const columnDefs = [
