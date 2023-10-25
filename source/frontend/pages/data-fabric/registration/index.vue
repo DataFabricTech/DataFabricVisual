40ce2453cd5f0285113c9e8a72967cb39e8e6cd0
codemotion19 <codemotion@mobigen.com>
2023-10-25 11:24:06 +0900

    [FAB2023-147] fix: button 태그 추가, 타이틀 추가

--- source/frontend/pages/data-fabric/registration/index.vue
+++ source/frontend/pages/data-fabric/registration/index.vue
@@ -8,75 +8,98 @@
         <div class="w-[900px] p-6">
           <BaseStep></BaseStep>
           <div class="storage-type">
+            <h4 class="storage-type-title">저장소 유형 선택</h4>
             <SearchField class="search-field-lg w-[440px]"></SearchField>
             <ul class="storage-type-list">
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_01.png" alt="mariaDB">
-                </div>
-                <span class="storage-type-title">MariaDB</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_01.png" alt="mariaDB">
+                  </div>
+                  <span class="storage-type-item-title">MariaDB</span>
+                </button>
               </li>
               <!-- item 요소 선택시 is-selected 클래스 추가 필요 -->
               <li class="storage-type-item is-selected">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_02.png" alt="MySQL">
-                </div>
-                <span class="storage-type-title">MySQL</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_02.png" alt="MySQL">
+                  </div>
+                  <span class="storage-type-item-title">MySQL</span>
+                </button>
               </li>
               <!-- 필요시 is-disabled 사용가능 -->
               <li class="storage-type-item is-disabled">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_03.png" alt="PostgreSQL">
-                </div>
-                <span class="storage-type-title">PostgreSQL</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_03.png" alt="PostgreSQL">
+                  </div>
+                  <span class="storage-type-item-title">PostgreSQL</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_04.png" alt="Oracle">
-                </div>
-                <span class="storage-type-title">Oracle</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_04.png" alt="Oracle">
+                  </div>
+                  <span class="storage-type-item-title">Oracle</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_05.png" alt="mariaDB">
-                </div>
-                <span class="storage-type-title">Hadoop</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_05.png" alt="mariaDB">
+                  </div>
+                  <span class="storage-type-item-title">Hadoop</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_06.png" alt="MinIO">
-                </div>
-                <span class="storage-type-title">MinIO</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_06.png" alt="MinIO">
+                  </div>
+                  <span class="storage-type-item-title">MinIO</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_07.png" alt="IRIS">
-                </div>
-                <span class="storage-type-title">IRIS</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_07.png" alt="IRIS">
+                  </div>
+                  <span class="storage-type-item-title">IRIS</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_08.png" alt="MongoDB">
-                </div>
-                <span class="storage-type-title">MongoDB</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_08.png" alt="MongoDB">
+                  </div>
+                  <span class="storage-type-item-title">MongoDB</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_06.png" alt="sample-01">
-                </div>
-                <span class="storage-type-title">sample-01</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_06.png" alt="sample-01">
+                  </div>
+                  <span class="storage-type-item-title">sample-01</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_07.png" alt="sample-02">
-                </div>
-                <span class="storage-type-title">sample-02 타이틀이 길어질때 말줄임 처리</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_07.png" alt="sample-02">
+                  </div>
+                  <span class="storage-type-item-title">sample-02 타이틀이 길어질때 말줄임 처리</span>
+                </button>
               </li>
               <li class="storage-type-item">
-                <div class="storage-type-img">
-                  <img src="../../../assets/images/storage-type_08.png" alt="sample-03">
-                </div>
-                <span class="storage-type-title">sample-03</span>
+                <button class="storage-type-button">
+                  <div class="storage-type-img">
+                    <img src="../../../assets/images/storage-type_08.png" alt="sample-03">
+                  </div>
+                  <span class="storage-type-item-title">sample-03</span>
+                </button>
               </li>
             </ul>
           </div>
@@ -96,9 +119,24 @@
 
     <h3>저장소 관리 > 연결정보 등록 Step 2</h3>
     <baseButton class="button-lg button-primary" @click="onClickOpen2">모달 OPEN</baseButton>
-    <modal v-model="show2" title="연결정보 등록 Step 2">
+    <modal v-model="show2" title="연결정보 등록">
       <template v-slot:body>
-        가나다라마바사~가나다라마바사~가나다라마바사~가나다라마바사~
+        <div class="w-[900px] p-6">
+          <BaseStep></BaseStep>
+          <div class="storage-type">
+            <h4>기본 정보 입력</h4>
+          </div>
+        </div>
+      </template>
+      <template v-slot:foot>
+        <baseButton class="button-normal button-lg">
+          <span class="button-text">취소</span>
+        </baseButton>
+        <div class="h-group ml-auto">
+          <baseButton class="button-primary button-lg">
+            <span class="button-text">다음</span>
+          </baseButton>
+        </div>
       </template>
     </modal>
   </div>
