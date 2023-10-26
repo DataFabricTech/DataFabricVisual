219c0744282dc6010d1b9f08a3ae0a36ae044641
ohmy04180 <ohmy0418@mobigen.com>
2023-10-27 08:59:59 +0900

    [FAB2023-129] feat: 데이터모델 브라우저 스타일 수정

--- source/frontend/pages/data-fabric/browse/index.vue
+++ source/frontend/pages/data-fabric/browse/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="l-split">
-    <h2 class="hidden">데이터모델 브라우저</h2>
+    <h2 class="hidden">데이터모델 Browse</h2>
     <section class="l-side">
       <h3 class="hidden">카테고리 | 카탈로그 | 연결정보</h3>
-      <div class="l-side-inner">
+      <div class="side-container">
         <!--      <BaseTab theme="fill-tab"></BaseTab> 으로 컴포넌트 사용 가능-->
         <div class="fill-tab">
           <ul class="fill-tab-list">
@@ -24,33 +24,39 @@
             </li>
           </ul>
         </div>
-        <ul class="list-list p-2">
-          <li class="list-item">
-            <button class="list-button" type="button">
-              <svg-icon class="svg-icon" name="object-list"></svg-icon>
-              <span class="list-text">전체 데이터 모델</span></button>
-          </li>
-          <li class="list-item">
-            <button class="list-button" type="button">
-              <svg-icon class="svg-icon" name="data"></svg-icon>
-              <span class="list-text">내가 등록한 모델</span></button>
-          </li>
-          <li class="list-item">
-            <button class="list-button" type="button">
-              <svg-icon class="svg-icon" name="bookmark"></svg-icon>
-              <span class="list-text">북마크한 모델</span></button>
-          </li>
-          <li class="list-item">
-            <button class="list-button" type="button">
-              <svg-icon class="svg-icon" name="write"></svg-icon>
-              <span class="list-text">최근 분석 데이터 모델</span></button>
-          </li>
-          <li class="list-item">
-            <button class="list-button" type="button">
-              <svg-icon class="svg-icon" name="time"></svg-icon>
-              <span class="list-text">최근 조회한 데이터 모델</span></button>
-          </li>
-        </ul>
+        <div class="side-wrapper">
+          <h4 class="hidden">카테고리 목록</h4>
+          <ul class="list-list p-2">
+            <li class="list-item">
+              <button class="list-button" type="button">
+                <svg-icon class="svg-icon" name="object-list"></svg-icon>
+                <span class="list-text">전체 데이터 모델</span></button>
+            </li>
+            <li class="list-item">
+              <button class="list-button" type="button">
+                <svg-icon class="svg-icon" name="data"></svg-icon>
+                <span class="list-text">내가 등록한 모델</span></button>
+            </li>
+            <li class="list-item">
+              <button class="list-button" type="button">
+                <svg-icon class="svg-icon" name="bookmark"></svg-icon>
+                <span class="list-text">북마크한 모델</span></button>
+            </li>
+            <li class="list-item">
+              <button class="list-button" type="button">
+                <svg-icon class="svg-icon" name="write"></svg-icon>
+                <span class="list-text">최근 분석 데이터 모델</span></button>
+            </li>
+            <li class="list-item">
+              <button class="list-button" type="button">
+                <svg-icon class="svg-icon" name="time"></svg-icon>
+                <span class="list-text">최근 조회한 데이터 모델</span></button>
+            </li>
+          </ul>
+        </div>
+        <div class="side-wrapper">
+          <h4 class="hidden">나의 카테고리</h4>
+        </div>
       </div>
     </section>
     <section class="l-split-section">
