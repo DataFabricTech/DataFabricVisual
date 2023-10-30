9723c9731b3070d3b81fe3b29e462d3b7650d355
jungseolhwa <dhqkfskrwl@naver.com>
2023-10-30 14:16:55 +0900

    [FAB2023-125]저장소관리 > 저장소 상세 퍼블리싱

--- source/frontend/pages/data-fabric/repository-detail/index.vue
+++ source/frontend/pages/data-fabric/repository-detail/index.vue
@@ -104,10 +104,18 @@
       <h3 class="hidden">선택된 데이터 정보</h3>
       <article class="page-article">
         <div class="card-top-bar">
-          <BaseSwitch>
-            <template v-slot:switch-text>데이터 모델 생성</template>
-            <template v-slot:hidden-text>데이터 모델 생성</template>
-          </BaseSwitch>
+          <div class="h-group gap-2">
+            <baseButton class="button-normal">
+              <svg-icon class="svg-icon" name="pen"></svg-icon>
+              <span class="button-text">수정</span>
+            </baseButton>
+            <baseButton class="button-normal">
+              <span class="button-text">권한 설정</span>
+            </baseButton>
+          </div>
+          <baseButton class="button-danger">
+            <span class="button-text">삭제</span>
+          </baseButton>
         </div>
         <Card class="w-full"></Card>
       </article>
@@ -115,71 +123,135 @@
         <div class="tab">
           <ul class="tab-list">
             <li class="tab-item">
+              <button type="button" class="tab-button">
+                <span class="tab-title">상태 정보</span>
+              </button>
+            </li>
+            <li class="tab-item">
               <button type="button" class="tab-button is-selected">
-                <span class="tab-title">Overview</span>
+                <span class="tab-title">기본 정보</span>
               </button>
             </li>
             <li class="tab-item">
               <button type="button" class="tab-button">
-                <span class="tab-title">Data</span>
-              </button>
-            </li>
-            <li class="tab-item">
-              <button type="button" class="tab-button">
-                <span class="tab-title">Query</span>
+                <span class="tab-title">수집 정보</span>
               </button>
             </li>
           </ul>
         </div>
         <div class="v-group gap-9 p-6 w-full">
           <div class="page-group">
-            <h4 class="page-subtitle">Summary</h4>
-            <div class="h-group gap-8 w-full">
-              <dl class="data-status" v-for="index in 3" >
-                <dt class="data-status-label">
-                  <svg-icon name="data" class="svg-icon"/>
-                  <span class="data-status-name">Connected Data</span>
-                </dt>
-                <dd class="data-status-num">8</dd>
-              </dl>
-            </div>
-          </div>
-          <div class="page-group">
-            <h4 class="page-subtitle">연관된 데이터 모델</h4>
-            <div class="card-container">
-              <ul class="card-simple-list">
-                <li class="card-simple-item" v-for="index in 3">
-                  <CardSimple></CardSimple>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="page-group">
-            <h4 class="page-subtitle">Structure</h4>
             <div class="table-col w-full">
               <table class="w-full">
                 <caption class="caption-out">
                   Description Column
                 </caption>
+                <colgroup>
+                  <col style="width:284px;" />
+                  <col />
+                </colgroup>
                 <thead>
                 <tr>
                   <!-- 세로 => col, 가로 => row -->
-                  <th scope="col">Field</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Labels</th>
-                  <th scope="col">Tags</th>
-                  <th scope="col">...</th>
-                  <th scope="col">...</th>
+                  <th scope="col">항목</th>
+                  <th scope="col">내용</th>
                 </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="index in 6">
-                    <td>acute_long_term_care_beds 1</td>
-                    <td>필드설명</td>
-                    <td>라벨정보</td>
-                    <td>태그정보</td>
-                    <td>...</td>
-                    <td>...</td>
+                  <tr>
+                    <td>연결정보이름</td>
+                    <td>연결정보B</td>
+                  </tr>
+                  <tr>
+                    <td>데이터 저장소 유형</td>
+                    <td>MySQL</td>
+                  </tr>
+                  <tr>
+                    <td>Driver (Adapter)</td>
+                    <td>MySQL 5</td>
+                  </tr>
+                  <tr>
+                    <td>Host</td>
+                    <td>foo.mobigen.com</td>
+                  </tr>
+                  <tr>
+                    <td>Port</td>
+                    <td>3305</td>
+                  </tr>
+                  <tr>
+                    <td>ID</td>
+                    <td>fooxxx</td>
+                  </tr>
+                  <tr>
+                    <td>Password</td>
+                    <td>xxxx</td>
+                  </tr>
+                  <tr>
+                    <td>Cert</td>
+                    <td>xxxx</td>
+                  </tr>
+                  <tr>
+                    <td>상태</td>
+                    <td>
+                      <baseBadge class="bg-marker-cyan">Active</baseBadge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>등록 데이터 / 전체 데이터</td>
+                    <td>1712 / 2201</td>
+                  </tr>
+                  <tr>
+                    <td>동기화 주기</td>
+                    <td>3일 / AM 01:30:00</td>
+                  </tr>
+                  <tr>
+                    <td>마지막 동기화 상태</td>
+                    <td>
+                      <baseBadge class="bg-marker-cyan">Success</baseBadge>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>마지막 동기화 시간</td>
+                    <td>2023-01-05  12:10:14</td>
+                  </tr>
+                  <tr>
+                    <td>소유자(등록자)</td>
+                    <td>User C</td>
+                  </tr>
+                  <tr>
+                    <td>공개 여부</td>
+                    <td>
+                      <div class="h-group gap-3">
+                        <baseRadio  id="radio-all" name="disclosure-status">전체 공개</baseRadio>
+                        <baseRadio  id="radio-group" name="disclosure-status">그룹 공개</baseRadio>
+                        <baseRadio  id="radio-none" name="disclosure-status">비공개</baseRadio>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>설명</td>
+                    <td>김아무개가 만든 MySQL 데이터 소스 저장소</td>
+                  </tr>
+                  <tr>
+                    <td>생성일시</td>
+                    <td>2023-01-04  12:13:14</td>
+                  </tr>
+                  <tr>
+                    <td>수정자</td>
+                    <td>홍길동</td>
+                  </tr>
+                  <tr>
+                    <td>수정일시</td>
+                    <td>2023-01-04  13:13:14</td>
+                  </tr>
+                  <tr>
+                    <td>태그</td>
+                    <td>
+                      <div class="h-group gap-3">
+                        <baseTag># 김아무개</baseTag>
+                        <baseTag># 김아무개</baseTag>
+                      </div>
+                    </td>
                   </tr>
                 </tbody>
               </table>
