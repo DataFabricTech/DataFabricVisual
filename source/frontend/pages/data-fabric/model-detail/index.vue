f0a2134c9bf401086ae9c59a74a138e63d06226e
ohmy04180 <ohmy0418@mobigen.com>
2023-10-31 17:15:17 +0900

    [FAB2023-130] test

--- source/frontend/pages/data-fabric/model-detail/index.vue
+++ source/frontend/pages/data-fabric/model-detail/index.vue
@@ -18,137 +18,126 @@
           <span class="button-text">수정</span>
         </BaseButton>
       </div>
-      <article class="page-article">
-        <div class="result-info">
-          <h4 class="result-summary-title">"<em class="color-tertiary">키워드</em>"검색 결과는 <em class="color-tertiary">4</em>건
-            입니다.</h4>
-          <div class="result-sort">
-            <BaseSelect class="select-lg"></BaseSelect>
-            <BaseSelect class="select-lg"></BaseSelect>
-          </div>
-        </div>
-        <ul class="card-list">
-          <li class="card-item" v-for="index in 10">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon class="svg-icon" name="data"></svg-icon>
-              </template>
-            </Card>
-          </li>
-        </ul>
-        <BasePagination></BasePagination>
-      </article>
-    </section>
-    <section class="l-section gap-[28px]">
+      <Card class="card-lg w-full"></Card>
       <div class="anchor" id="all">
         <a class="anchor-link" title="이동" href="#all">전체 (16)</a>
         <a class="anchor-link" title="이동" href="#data-model">데이터모델 (6)</a>
         <a class="anchor-link" title="이동" href="#repository">저장소 (5)</a>
         <a class="anchor-link" title="이동" href="#domain">도메인 (5)</a>
       </div>
-      <div class="result-info">
-        <h3 class="result-summary-title">"<em class="color-tertiary">키워드</em>"검색 결과는 <em class="color-tertiary">4</em>건
-          입니다.</h3>
-        <div class="result-sort">
-          <div class="toggle toggle-icon toggle-lg">
-            <BaseRadio name="rI" id="ic-01">
-              <span class="hidden">리스트형 보기</span>
-              <svg-icon name="list-view" class="svg-icon"></svg-icon>
-            </BaseRadio>
-            <BaseRadio name="rI" id="ic-03">
-              <span class="hidden">썸네일형 보기</span>
-              <svg-icon name="thumbnail-view" class="svg-icon"></svg-icon>
-            </BaseRadio>
-          </div>
-          <BaseSelect class="select-lg"></BaseSelect>
-          <BaseSelect class="select-lg"></BaseSelect>
+      <article class="category-article">
+        <h4 class="page-subtitle">최근 등록/변경된 연결정보</h4>
+        <div class="table-col w-full">
+          <table class="w-full">
+            <caption class="caption-out">
+              Description Column
+            </caption>
+            <thead>
+            <tr>
+              <!-- 세로 => col, 가로 => row -->
+              <th scope="col">연결정보 이름</th>
+              <th scope="col">데이터 저장소 유형</th>
+              <th scope="col">상태</th>
+              <th scope="col">소유자(등록자)</th>
+              <th scope="col">등록일시</th>
+              <th scope="col">변경일시</th>
+              <th scope="col">내용</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>Inactive</td>
+              <td>User A</td>
+              <td>2023-01-02 12:13:14</td>
+              <td>2023-01-05 12:13:14</td>
+              <td>수정</td>
+            </tr>
+            <tr>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>Inactive</td>
+              <td>User A</td>
+              <td>2023-01-02 12:13:14</td>
+              <td>2023-01-05 12:13:14</td>
+              <td>수정</td>
+            </tr>
+            <tr>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>Inactive</td>
+              <td>User A</td>
+              <td>2023-01-02 12:13:14</td>
+              <td>2023-01-05 12:13:14</td>
+              <td>수정</td>
+            </tr>
+            <tr>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>Inactive</td>
+              <td>User A</td>
+              <td>2023-01-02 12:13:14</td>
+              <td>2023-01-05 12:13:14</td>
+              <td>수정</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
-      </div>
-      <article class="category-article ">
-        <div class="category-top-bar">
-          <h4 class="category-name" id="data-model">데이터모델 (6)</h4>
-        </div>
-        <ul class="card-list">
-          <!--              선택된 데이터모델 is-selected 클래스 추가-->
-          <li class="card-item is-selected">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-negative">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-negative">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-negative">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-negative">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-primary">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-          <li class="card-item">
-            <Card class="card-negative">
-              <template v-slot:img>
-                <svg-icon name="data" class="svg-icon"></svg-icon>
-              </template>
-            </Card>
-          </li>
-        </ul>
-        <BasePagination></BasePagination>
       </article>
       <article class="category-article">
-        <div class="category-top-bar">
-          <h4 class="category-name" id="data-model">데이터모델 (6)</h4>
-        </div>
-        <div class="no-result">
-          <storng class="no-result-title">검색 결과가 없습니다.</storng>
+        <h4 class="page-subtitle">히스토리</h4>
+        <div class="table-col w-full">
+          <table class="w-full">
+            <caption class="caption-out">
+              Description Column
+            </caption>
+            <thead>
+            <tr>
+              <!-- 세로 => col, 가로 => row -->
+              <th scope="col">이벤트 발생 시간</th>
+              <th scope="col">이벤트</th>
+              <th scope="col">연결정보 이름</th>
+              <th scope="col">데이터 저장소 유형</th>
+              <th scope="col">소유자(등록자)</th>
+              <th scope="col">내용</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>2023-01-05 12:13:14</td>
+              <td>연결 끊김</td>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>User A</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>2023-01-05 12:13:14</td>
+              <td>연결 끊김</td>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>User A</td>
+              <td>자동 등록 데이터 xx</td>
+            </tr>
+            <tr>
+              <td>2023-01-05 12:13:14</td>
+              <td>연결 끊김</td>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>User A</td>
+              <td>자동 등록 데이터 xx</td>
+            </tr>
+            <tr>
+              <td>2023-01-05 12:13:14</td>
+              <td>연결 끊김</td>
+              <td>연결정보 H</td>
+              <td>MySQL</td>
+              <td>User A</td>
+              <td>자동 등록 데이터 xx</td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </article>
     </section>
