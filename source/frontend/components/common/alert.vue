2a820562f961f2c5f42fcbb847806f1f22d82e1c
codemotion19 <codemotion@mobigen.com>
2023-11-01 15:56:46 +0900

    [FAB2023-306] feat: Alert UI 컴포넌트 추가

--- source/frontend/components/common/alert.vue
+++ source/frontend/components/common/alert.vue
@@ -1,6 +1,30 @@
 <template>
-  <h1>alert 컴포넌트</h1>
+  <div class="alert" :class="'alert-'+props.theme">
+    <div class="alert-body">
+      <svg-icon :name="props.theme" class="svg-icon" />
+      <div class="alert-contents">
+        <p class="alert-text">
+          이 얼럿은 사용자에게 정보 상태를 알리기 위해 사용합니다.
+        </p>
+      </div>
+    </div>
+    <div class="alert-foot">
+      <div class="alert-foot-main-buttons">
+        <baseButton>
+          <span class="button-text">취소</span>
+        </baseButton>
+        <slot>
+          <baseButton class="button-lg button-primary">
+            <span class="button-text">확인</span>
+          </baseButton>
+        </slot>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+const props = defineProps({
+  theme: { type: String, default: "normal" }
+});
 </script>
