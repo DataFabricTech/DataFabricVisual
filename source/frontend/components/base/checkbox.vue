d8605bdeab0b5df21ef6c080d1ab61c0d6710c5b
leejeongkang <krj1226@gmail.com>
2023-11-01 18:19:15 +0900

    [FAB2023-300] feat: 리스트 컴포넌트 개발 - check ver

--- source/frontend/components/base/checkbox.vue
+++ source/frontend/components/base/checkbox.vue
@@ -7,21 +7,27 @@
       :name="props.name"
       :disabled="props.disabled"
       :checked="props.checked"
+      :value="props.value"
       @change="$emit('change', $event.target.checked)"
     >
     <label :for="props.id" class="checkbox-label">
-      <slot></slot>
+      <slot>{{ props.name }}</slot>
     </label>
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineEmits } from "vue/dist/vue";
 
 const props = defineProps({
-  id: { type: String, default: null},
+  id: { type: String || Number, default: null },
   name: { type: String, default: null},
   disabled: { type: Boolean, default: false },
   checked: { type: Boolean, default: false },
+  value: { default: null}
 });
+const emit = defineEmits<{
+  change: [checked: boolean]
+}>()
 
 </script>
--- source/frontend/components/base/select.vue
+++ source/frontend/components/base/select.vue
@@ -1,10 +1,9 @@
 <template>
-  <!-- 현재 위치에서 클릭시 is-open 클래스 추가 -->
   <div :class="toggle === false ? 'select' : 'select is-open'" @click="openToggle">
     <div class="select-selector" role="combobox" aria-expanded="false" aria-haspopup="true">
       <button class="select-selector-button" type="button" title="열기">
         <slot name="title">
-          <span class="select-selector-title">{{ setSelect }}</span>
+          <span class="select-selector-title">{{ props.isCheck ? setCheckTitle : setSelectTitle }}</span>
         </slot>
         <span class="hidden">선택</span>
         <svg-icon class="svg-icon select-selector-icon" name="chevron-down-medium" aria-hidden="true"></svg-icon>
@@ -17,14 +16,22 @@
       </baseButton>
     </div>
     <div class="select-container" id="select-01">
-      <ul class="select-container-list" role="listbox" v-for="(item, index) in props.data">
+      <ul class="select-container-list" role="listbox">
         <li class="select-container-item" v-if="props.isCheck">
-          <baseCheckbox class="checkbox-indeterminate" role="option">전체</baseCheckbox>
+          <baseCheckbox class="checkbox-indeterminate" role="option" @change="checkAll" :id="'checkAll'" :checked="isAllCheck">전체</baseCheckbox>
         </li>
-        <li class="select-container-item" v-if="props.isCheck">
-          <baseCheckbox role="option">item 01</baseCheckbox>
+        <li class="select-container-item" v-if="props.isCheck" v-for="(item, index) in props.data">
+          <baseCheckbox
+            role="option"
+            :id="index + props.checkBoxId.toString()"
+            :name="item[dataKey]"
+            :value="item[dataValue]"
+            :checked="checkList.includes(item[dataValue])"
+            :disabled="false"
+            @change="(checked) => checkData(checked, item, index)"
+          ></baseCheckbox>
         </li>
-        <li class="select-container-item" v-if="!props.isCheck">
+        <li class="select-container-item" v-if="!props.isCheck" v-for="(item, index) in props.data">
           <button class="select-container-button" type="button" role="option" @click="clickItem(item)">
             <span class="select-container-text">{{ item[dataKey] }}</span>
           </button>
@@ -65,29 +72,94 @@ const props = defineProps({
   isCheck: {
     type: Boolean,
     default: false
+  },
+  checkBoxId: {
+    type: Number,
+    default: Math.random()
   }
 });
-const emit = defineEmits(['select']);
+const emit = defineEmits<{
+  select: [data: any]
+}>()
+
 const toggle = ref(false);
 const select = ref(null);
+const checkList :any = ref([]);
+const isAllCheck = ref(false);
+const checkMsg = ref("선택");
 function openToggle() {
   toggle.value = !toggle.value
 }
-const setSelect = computed(() => {
-  if(props.data !== null) {
-    if (select.value !== null) {
-      return select.value;
-    } else {
-      let item : data = props.data[0]
-      clickItem(item);
-      return item[props.dataKey];
-    }
-  } else {
-    return props.title
+
+/**
+ * 일반 셀렉트 박스일 경우 선택 값
+ */
+const setSelectTitle = computed(() => {
+  if (props.data === null) {
+    return props.title;
   }
+  if (!props.isCheck && select.value !== null) {
+    return select.value;
+  }
+  const defaultItem = props.data[0];
+  clickItem(defaultItem);
+  return defaultItem[props.dataKey];
+
 });
+/**
+ * 체크 셀렉트 박스일 경우 선택 값
+ */
+const setCheckTitle = computed(() => {
+  const length = checkList.value.length;
+  if (length === props.data?.length) {
+    return "전체";
+  } else if (length >= 2 && length < props.data?.length) {
+    return `다중 선택(${length})`;
+  } else if (length === 1) {
+    for(let item of props.data) {
+      if(item[props.dataValue] === checkList.value[0]) {
+        return item[props.dataKey]
+      }
+    }
+  } else if (length === 0) {
+    return "선택";
+  }
+})
+/**
+ * 일반 셀렉트 박스 선택 기능
+ * @param item
+ */
 const clickItem = (item: data) => {
   select.value = item[props.dataKey];
   emit('select', item[props.dataValue]);
 };
+
+/**
+ * 체크 셀렉트 박스 전체 선택 기능
+ * @param checked
+ */
+function checkAll(checked: boolean) {
+  isAllCheck.value = checked;
+  checkList.value = checked ? props.data.map(item => item[props.dataValue]) : [];
+  checkMsg.value = checked ? "전체" : "선택";
+}
+
+/**
+ * 체크 셀렉트 박스 선택 기능
+ * @param checked
+ * @param item
+ * @param index
+ */
+function checkData(checked: boolean, item: data, index: number) {
+  if(checked) {
+    checkList.value.push(item[props.dataValue])
+  } else {
+    let indexToRemove = checkList.value.indexOf(item[props.dataValue]);
+    if (indexToRemove !== -1) {
+      checkList.value.splice(indexToRemove, 1);
+    }
+  }
+  isAllCheck.value = checkList.value.length == props.data?.length
+  emit('select', checkList.value);
+}
 </script>
