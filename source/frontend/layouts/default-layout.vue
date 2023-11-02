0fbecfbf5eee2b811ccb9476fdc09948c88a4270
lyj <eojoo00@gmail.com>
2023-11-02 10:27:05 +0900

    fix: build 에러 수정

--- source/frontend/layouts/default-layout.vue
+++ source/frontend/layouts/default-layout.vue
@@ -11,10 +11,6 @@
 </template>
 
 <script lang="ts" setup>
-import TheHeader from "./data-fabric/the-header.vue";
-import TheTab from "./data-fabric/the-tab.vue";
-
-export default {
-  components: { TheHeader, TheTab }
-};
+import TheHeader from "/layouts/data-fabric/the-header.vue";
+import TheTab from "/layouts/data-fabric/the-tab.vue";
 </script>
