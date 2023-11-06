143e2ba3751e68fb28cd22d05042f19695df35ac
lyj <eojoo00@gmail.com>
2023-11-06 17:40:17 +0900

    chore: componenet-ui - tree 샘플 데이터 세팅

--- source/frontend/pages/component-ui.vue
+++ source/frontend/pages/component-ui.vue
@@ -17,8 +17,8 @@
               <div class="v-group gap-5">
                 <div class="h-group gap-2">
                   <strong class="w-[150px] font-light text-[14px] shrink-0">default</strong>
-                    <CommonTree v-bind="args" v-on="$props">
-                    </CommonTree>
+                    <Tree :nodes="treeNodes" :config="treeConfig">
+                    </Tree>
                 </div>
               </div>
             </td>
@@ -1229,77 +1229,80 @@ const args = {
   dateRange: ["2023-11-10", "2023-11-22"],
   dateType: "time",
   rangeType: "datetime",
-  node: {
-    id1: {
-      text: "text1",
-      children: ["id11", "id12"],
-    },
-    id11: {
-      text: "text11",
-    },
-    id12: {
-      text: "text12",
-    },
-    id2: {
-      text: "text2",
-    },
-    id3: {
-      text: "text3",
-      children: ["id31"],
-      state: {
-        opened: true
-      }
-    },
-    id31: {
-      text: "text31",
-      children: ["id311", "id312", "id313"],
-      state: {
-        opened: true,
-      }
-    },
-    id311: {
-      text: "text311",
-      state: {
-        checked: true
-      }
-    },
-    id312: {
-      text: "text312",
-      state: {
-        disabled: true,
-        checked: true
-      }
-    },
-    id313: {
-      text: "text313",
-      state: {
-        editable: true
-      }
-    },
-    id4: {
-      text: "text4",
-    },
-  },
-  config: {
-    "roots": ["id1", "id2", "id3"],
-    openedIcon: {
-      type: "shape",
-      stroke: "black",
-      strokeWidth: 3,
-      viewBox: "0 0 24 24",
-      draw: "M 2 12 L 22 12",
-    },
-    closedIcon: {
-      type: "shape",
-      stroke: "black",
-      strokeWidth: 3,
-      viewBox: "0 0 24 24",
-      draw: `M 12 2 L 12 22 M 2 12 L 22 12`,
-    },
-    checkboxes: true,
-    editable: true
-  }
 };
+
+// tree 변수
+const treeNodes = ref({
+  id1: {
+    text: "text1",
+    children: ["id11", "id12"],
+  },
+  id11: {
+    text: "text11",
+  },
+  id12: {
+    text: "text12",
+  },
+  id2: {
+    text: "text2",
+  },
+  id3: {
+    text: "text3",
+    children: ["id31"],
+    state: {
+      opened: true
+    }
+  },
+  id31: {
+    text: "text31",
+    children: ["id311", "id312", "id313"],
+    state: {
+      opened: true,
+    }
+  },
+  id311: {
+    text: "text311",
+    state: {
+      checked: true
+    }
+  },
+  id312: {
+    text: "text312",
+    state: {
+      disabled: true,
+      checked: true
+    }
+  },
+  id313: {
+    text: "text313",
+    state: {
+      editable: true
+    }
+  },
+  id4: {
+    text: "text4",
+  },
+});
+
+const treeConfig = ref({
+  roots: ["id1", "id2", "id3"],
+  openedIcon: {
+    type: "shape",
+    stroke: "black",
+    strokeWidth: 3,
+    viewBox: "0 0 24 24",
+    draw: "M 2 12 L 22 12",
+  },
+  closedIcon: {
+    type: "shape",
+    stroke: "black",
+    strokeWidth: 3,
+    viewBox: "0 0 24 24",
+    draw: `M 12 2 L 12 22 M 2 12 L 22 12`,
+  },
+  checkboxes: true,
+  editable: true
+});
 
 function onClickOpen() {
   show.value = !show.value;
