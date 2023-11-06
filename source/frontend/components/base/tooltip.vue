862179c3663b9e00591538f4685b3ca73d444d43
jungseolhwa <dhqkfskrwl@naver.com>
2023-11-06 15:46:25 +0900

    [FAB2023-322]아이콘 추가, 툴팁컴포넌트 추가

---
+++ source/frontend/components/base/tooltip.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tooltip">
+    <span class="tooltip-arrow"></span>
+    <div class="tooltip-inner">
+      <strong class="tooltip-title">
+        <slot name="title"></slot>
+      </strong>
+      <span class="tooltip-text">
+        <slot name="text"></slot>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
