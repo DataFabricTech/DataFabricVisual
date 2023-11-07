f8eaebbd744e0d788c8b0f0d0298aee854fb6462
김지혜 <kimjihye6245@naver.com>
2023-11-07 16:53:06 +0900

    feat: 최근 검색어 닫기 emit

--- source/frontend/components/base/tag-removable.vue
+++ source/frontend/components/base/tag-removable.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="tag-removable">
+  <div class="tag-removable" :disabled="disable">
     <button class="tag">
       <slot></slot>
     </button>
     <button class="button button-icon button-sm">
-      <svg-icon name="close" class="svg-icon" />
+      <svg-icon name="close" class="svg-icon" @click="closeTag"/>
     </button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emit = defineEmits(['close']);
+let disable = ref('false');
+function closeTag() {
+  disable.value = 'true';
+  console.log("show값 ? ", disable);
+  emit('close')
+}
+
+</script>
 
