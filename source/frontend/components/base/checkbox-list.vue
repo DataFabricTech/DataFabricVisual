05379426d24c60bc3ebe3943122abf7aa50fec58
sominJoo <js06m13@gmail.com>
2023-11-13 17:55:02 +0900

    [FAB2023-283] 연결정보 리스트 - 저장소 상세 필터 텍스트박스 추가

--- source/frontend/components/base/checkbox-list.vue
+++ source/frontend/components/base/checkbox-list.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="list">
-    <slot name="head"></slot>
+    <slot name="head">
+      <div class="list-head">
+        <div class="list-head-title">{{ title }}</div>
+        <base-search-text
+          v-if="props.useSearchFilter"
+          v-model:text="data.textFilter"
+          @reset="data.textFilter = ''"
+        ></base-search-text>
+      </div>
+    </slot>
     <div class="list-body">
       <slot name="options" v-if="enableCheckAll">
         <div class="list-options">
-          <baseCheckbox id="list-check-04" :checked="data.isAllChecked" @change="checkAllItem">전체</baseCheckbox>
+          <baseCheckbox id="list-check-04" :checked="data.isAllChecked" @change="onChangeAll">전체</baseCheckbox>
           <div class="list-options-count">
             <strong class="list-options-count-selected">{{ checkedList.length }}</strong>
             <span>/</span>
-            <span class="list-options-count-all"> {{ data.listData.length }} </span>
+            <span class="list-options-count-all"> {{ filteredList.length }} </span>
           </div>
         </div>
       </slot>
       <ul class="list-list">
-        <li class="list-item" v-for="(item, key) in data.listData" :key="key">
+        <li class="list-item" v-for="(item, key) in filteredList" :key="key">
           <baseCheckbox
             :id="'list-check' + item.value + '_' + key"
             :disabled="item.disable"
@@ -32,6 +41,14 @@
 import type { BaseCheckBoxListItem } from "~/components/base/checkbox-list";
 
 const props = defineProps({
+  title: {
+    type: String,
+    default: "Sample"
+  },
+  useSearchFilter: {
+    type: Boolean,
+    default: false
+  },
   enableCheckAll: {
     type: Boolean,
     default: true
@@ -42,30 +59,20 @@ const props = defineProps({
       return [];
     }
   },
-  checkedItems: {
-    type: Array<string>,
-    default: () => {
-      return [];
-    }
-  },
-  disabledItems: {
-    type: Array<string>,
-    default: () => {
-      return [];
-    }
-  },
-  valueKey : {
+  valueKey: {
     type: String,
     default: "value"
   }
 });
 
 const data: {
-  listData: Array<BaseCheckBoxListItem>;
+  listData: Array<any>;
   isAllChecked: boolean;
+  textFilter: string;
 } = reactive({
   listData: [],
-  isAllChecked: false
+  isAllChecked: false,
+  textFilter: ""
 });
 
 watch(
@@ -79,78 +86,75 @@ watch(
   { immediate: true, deep: true }
 );
 watch(
-  () => props.checkedItems,
-  (nValue, oValue) => {
-    if (_isEqual(nValue, oValue)) {
-      return;
-    }
-    applyChecked(nValue);
-  },
-  { immediate: true, deep: true }
-);
-watch(
-  () => props.disabledItems,
-  (nValue, oValue) => {
-    if (_isEqual(nValue, oValue)) {
-      return;
-    }
-    applyDisabled(nValue);
-  },
-  { immediate: true, deep: true }
+  () => data.textFilter,
+  () => {
+    emitChange();
+  }
 );
 
+/**
+ * 내부에서 사용되는 List 생성
+ */
 function makeList() {
   data.listData = _map(props.items, (item) => {
     return {
       name: item.name,
       value: item[props.valueKey],
-      disable: false,
-      checked: false
+      disable: item.disable,
+      checked: item.checked
     };
-  });
-  applyChecked(null);
-  applyDisabled(null);
-}
-
-function applyChecked(checkedList: Array<any>) {
-  checkedList = checkedList || props.checkedItems;
-
-  _forEach(data.listData, (item) => {
-    item.checked = _includes(checkedList, item.value);
   });
   data.isAllChecked = _every(data.listData, "checked");
 }
 
-function applyDisabled(disabledList: Array<any>) {
-  disabledList = disabledList || props.disabledItems;
+/**
+ * filter 된 리스트
+ */
+const filteredList = computed(() => {
+  return _filter(data.listData, (item) => _includes(item.name.toLowerCase(), data.textFilter.toLowerCase()));
+});
 
-  _forEach(data.listData, (item) => {
-    item.disable = _includes(disabledList, item.value);
-  });
-}
+/**
+ * check + filter 된 리스트
+ */
+const checkedList = computed(() => {
+  return _chain(filteredList.value)
+    .filter((item) => {
+      return item.checked;
+    })
+    .map(props.valueKey)
+    .value();
+});
 
-const emit = defineEmits<{
-  (e: "change", list: Array<string>): void;
-}>();
-function checkAllItem(checked: boolean) {
-  data.isAllChecked = checked;
+/**
+ * 전체 선택/해제
+ * @param checked
+ */
+function onChangeAll(checked: boolean) {
   _forEach(data.listData, (item) => {
     item.checked = checked;
   });
-  emit("change", checkedList.value);
+  data.isAllChecked = checked;
+  emitChange();
 }
+
+/**
+ * 단일 아이템 선택/해제
+ * @param item
+ * @param checked
+ */
 function checkItem(item: BaseCheckBoxListItem, checked: boolean) {
   _forEach(data.listData, (el) => {
     if (item.value === el.value) {
       el.checked = checked;
     }
   });
-  data.isAllChecked = _every(data.listData, "checked");
-  emit("change", checkedList.value);
+  data.isAllChecked = _every(filteredList, ["checked"]);
+  emitChange();
 }
 
-const checkedList = computed(() => {
-  // NOTE: id 값 출력
-  return _chain(data.listData).filter({ checked: true }).map(props.valueKey).value();
-});
+const emit = defineEmits<(e: "change", list: Array<string>) => void>();
+function emitChange() {
+  emit("change", checkedList.value);
+}
 </script>
