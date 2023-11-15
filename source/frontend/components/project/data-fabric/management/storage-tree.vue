e9b90725d9f244d766f96e4aa92a9c8b1d6dccb7
ohmy04180 <ohmy0418@mobigen.com>
2023-11-15 15:40:59 +0900

    [FAB2023-331] refactor: overview 페이지 이슈처리

--- source/frontend/components/project/data-fabric/management/storage-tree.vue
+++ source/frontend/components/project/data-fabric/management/storage-tree.vue
@@ -30,7 +30,7 @@
       <baseTextInput v-model:model-value="storage.filter.name" placeholder="연결정보 이름을 입력하세요"></baseTextInput>
     </div>
     <div class="search-tree">
-      <div class="list list-lg h-full justify-start" >
+      <div class="list list-lg" >
         <!--<ul class="list-list h-full">
           <li class="list-item" v-for="(item, key) in storageList" :key="key">
             <button class="list-button" v-if="item.show">
