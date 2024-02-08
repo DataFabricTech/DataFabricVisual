b394e2efaf21c40f27f50322862c0bc94df5076d
jungseolhwa <dhqkfskrwl@naver.com>
2024-02-08 17:28:07 +0900

    [FAB2023-574] init: 퍼블리싱 환경 세팅

--- project/ui/frontend/common/pages/index.vue
+++ project/ui/frontend/common/pages/index.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
     <h1>
-      Component
+      Fabric-design
     </h1>
+    <section class="guide-section">
+      <h2 class="section-title">color system</h2>
+      <ol class="test">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </section>
+    <section class="guide-section">
+      <h2 class="section-title">shadows</h2>
+      <ol class="test shadows">
+        <li>shadow-sm</li>
+        <li>shadow-md</li>
+        <li>shadow-lg</li>
+        <li>shadow-xl</li>
+      </ol>
+    </section>
+    <section class="guide-section">
+      <h2 class="section-title">typography</h2>
+      <div class="">
+        <p class="guide-typo">데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
+        <p class="guide-typo">데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
+        <p class="guide-typo">데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
+        <p class="guide-typo">데이터패브릭에서 제작한 모든 서비스와 문서에는 Pretendard 폰트를 사용합니다.</p>
+      </div>
+    </section>
+
+
+
+
     <ul>
       <li>
         <NuxtLink to="/dayjs">Dayjs</NuxtLink>
@@ -28,25 +61,27 @@
 </script>
 
 <style lang="css" scoped>
-ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-}
 
-li {
-  margin-right: 10px;
-  padding: 0;
-  border: 0;
-  float: left;
-  color: white;
-  font-size: 30px;
-  color: black;
-}
 
-li:hover {
-  background-color: #98E0AD;
-  color: black;
-}
+/*ul {*/
+/*  list-style: none;*/
+/*  margin: 0;*/
+/*  padding: 0;*/
+/*  display: flex;*/
+/*}*/
+
+/*li {*/
+/*  margin-right: 10px;*/
+/*  padding: 0;*/
+/*  border: 0;*/
+/*  float: left;*/
+/*  color: white;*/
+/*  font-size: 30px;*/
+/*  color: black;*/
+/*}*/
+
+/*li:hover {*/
+/*  background-color: #98E0AD;*/
+/*  color: black;*/
+/*}*/
 </style>
