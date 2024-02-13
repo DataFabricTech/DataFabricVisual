b1ba8d13a857b6033396d66349296bb2c05e8457
sominJoo <js06m13@gmail.com>
2024-02-13 11:17:05 +0900

    [FAB2023-580] fix: config > storybook 모듈 삭제

--- project/ui/frontend/common/nuxt.config.ts
+++ project/ui/frontend/common/nuxt.config.ts
@@ -1,64 +1,64 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
+  devtools: { enabled: true },
 
-  modules: ['@nuxtjs/storybook', 'nuxt-lodash', 'dayjs-nuxt'],
+  modules: ["nuxt-lodash", "dayjs-nuxt"],
 
   plugins: [
-    {src: '~/plugins/highcharts-vue', mode: 'client'},
-    {src: '~/plugins/vue-final-modal', mode: 'client'},
-    {src: '~/plugins/ag-grid-vue', mode: 'client'},
-    {src: "~/plugins/vue-datepicker-next", mode: "client"}
+    { src: "~/plugins/highcharts-vue", mode: "client" },
+    { src: "~/plugins/vue-final-modal", mode: "client" },
+    { src: "~/plugins/ag-grid-vue", mode: "client" },
+    { src: "~/plugins/vue-datepicker-next", mode: "client" }
   ],
 
   components: [
     {
       path: "~/components",
       pathPrefix: false
-    },
+    }
   ],
 
   css: [
-    '~/assets/css/main.scss',
-    'vue-final-modal/style.css',
-    'ag-grid-community/dist/styles/ag-grid.css',
-    'ag-grid-community/dist/styles/ag-theme-alpine.css'
+    "~/assets/css/main.scss",
+    "vue-final-modal/style.css",
+    "ag-grid-community/dist/styles/ag-grid.css",
+    "ag-grid-community/dist/styles/ag-theme-alpine.css"
   ],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-    },
+      autoprefixer: {}
+    }
   },
 
   lodash: {
-    prefix: '$_', // lodash 함수에 붙일 접두사
+    prefix: "$_", // lodash 함수에 붙일 접두사
     prefixSkip: [],
     upperAfterPrefix: false,
     exclude: [],
     alias: []
   },
   dayjs: {
-    locales: ['ko', 'en'],
+    locales: ["ko", "en"],
     plugins: [
-      'relativeTime',
-      'utc',
-      'timezone',
-      'timezone',
-      'duration',
-      'isBetween',
-      'IsLeapYear',
-      'IsSameOrAfter',
-      'IsSameOrBefore',
-      'isToday',
-      'isTomorrow',
-      'IsYesterday',
-      'objectSupport',
-      'pluralGetSet',
-      'toArray',
-      'toObject'
+      "relativeTime",
+      "utc",
+      "timezone",
+      "timezone",
+      "duration",
+      "isBetween",
+      "IsLeapYear",
+      "IsSameOrAfter",
+      "IsSameOrBefore",
+      "isToday",
+      "isTomorrow",
+      "IsYesterday",
+      "objectSupport",
+      "pluralGetSet",
+      "toArray",
+      "toObject"
     ],
-    defaultLocale: 'ko',
-    defaultTimezone: 'Asia/Seoul',
+    defaultLocale: "ko",
+    defaultTimezone: "Asia/Seoul"
   }
-})
+});
