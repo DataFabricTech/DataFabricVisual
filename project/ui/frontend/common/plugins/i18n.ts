a910c7c2608eebff0e2dd0c1e709a902e3bb4e85
황인용 <hiy2259@me.com>
2024-02-14 16:58:46 +0900

    [FAB2023-488] feat: i18n 설정 추가

---
+++ project/ui/frontend/common/plugins/i18n.ts
@@ -0,0 +1,19 @@
+import { createI18n } from "vue-i18n";
+import { defineNuxtPlugin } from "#app";
+
+import en from "~/locales/en.json";
+import ko from "~/locales/ko.json";
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  const i18n = createI18n({
+    legacy: false,
+    globalInjection: true,
+    locale: "ko",
+    messages: {
+      ko,
+      en
+    }
+  });
+
+  vueApp.use(i18n);
+});
