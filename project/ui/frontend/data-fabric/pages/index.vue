e597b76218eea3c6cfee513ef8c6430846351d63
황인용 <hiy2259@me.com>
2024-02-15 10:17:22 +0900

    [FAB2023-488] feat: 데이터패브릭 i18n 설정 추가

--- project/ui/frontend/data-fabric/pages/index.vue
+++ project/ui/frontend/data-fabric/pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <h2 class="test">
-    DATA Fabric
-  </h2>
+  <section>
+    <h2>DATA Fabric</h2>
+    <base-button>{{ t("test") }}</base-button>
+    {{ gt("common") }}
+  </section>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import messages from "./index.json";
+
+import BaseButton from "@base/button/button.vue";
+
+const { t } = useI18n({
+  messages
+});
+
 </script>
 
 <style lang="scss" scoped>
