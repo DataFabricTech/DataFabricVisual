07c851a8c0e14a14070cb9ce96fbd3afe95ee48a
jungseolhwa <dhqkfskrwl@naver.com>
2024-03-08 16:54:23 +0900

    [FAB2023-592] Breadcrumb title 추가

--- project/ui/frontend/common/components/base/breadcrumb/Breadcrumb.vue
+++ project/ui/frontend/common/components/base/breadcrumb/Breadcrumb.vue
@@ -3,15 +3,15 @@
     <ol class="breadcrumb-list">
       <li class="breadcrumb-item">
         <svg-icon class="svg-icon" name="home"></svg-icon>
-        <a href="#" class="breadcrumb-link">Breadcrumb</a>
+        <a href="#" class="breadcrumb-link" title="이동">Breadcrumb</a>
       </li>
       <li class="breadcrumb-item">
         <svg-icon class="svg-icon" name="home"></svg-icon>
-        <a href="#" class="breadcrumb-link">Breadcrumb</a>
+        <a href="#" class="breadcrumb-link" title="이동">Breadcrumb</a>
       </li>
       <li class="breadcrumb-item is-selected">
         <svg-icon class="svg-icon" name="home"></svg-icon>
-        <a href="#" class="breadcrumb-link">Breadcrumb</a>
+        <a href="#" class="breadcrumb-link" title="이동">Breadcrumb</a>
       </li>
     </ol>
   </div>
