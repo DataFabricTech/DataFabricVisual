73f4d48531a1db7fc2087bc671ba5bda110f58f2
김지혜 <kimjihye6245@naver.com>
2024-03-20 16:55:14 +0900

    [FAB2023-637] feat: Combobox - 콤보박스컴포넌트 개발

---
+++ project/ui/frontend/common/components/extends/combo-box/ComboBox.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="combo-box" style="width: 300px; height: 30px" v-on-click-outside="closeDropdown">
+    <input
+      type="text"
+      @click="toggleList"
+      v-model="selectedLabel"
+      :placeholder="placeholder"
+      style="width: 100%; height: 100%"
+    />
+    <ul v-show="isShowBox">
+      <li
+        v-for="(option, index) in filteredOptions"
+        :key="index"
+        @click="selectItem(option)"
+        :class="[{ 'disabled-option': isDisabled(option[valueKey]) }, { active: isActive(option[valueKey]) }]"
+      >
+        {{ option[labelKey] }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { vOnClickOutside } from "@vueuse/components";
+import { ComboBoxProps } from "@/components/extends/combo-box/ComboBoxProps";
+import { ComboboxComposition } from "@/components/extends/combo-box/ComboBoxComposition";
+
+const props = withDefaults(defineProps<ComboBoxProps>(), {
+  data: () => [],
+  labelKey: "label",
+  valueKey: "value",
+  iconKey: "icon",
+  selectedItem: undefined,
+  disabledList: () => [],
+  disabledAll: false,
+  nodataMsg: "데이터가 없습니다.",
+  placeholder: "입력",
+  isFirstSelectedEvent: true
+});
+
+const emit = defineEmits<{ (e: "select", option: number | string): void }>();
+function onSelect(value: string | number) {
+  emit("select", value);
+}
+
+const {
+  data,
+  labelKey,
+  valueKey,
+  filteredOptions,
+  isShowBox,
+  selectedLabel,
+  toggleList,
+  selectItem,
+  isDisabled,
+  placeholder,
+  isActive,
+  closeDropdown
+} = ComboboxComposition(props, onSelect);
+</script>
+
+<style>
+.select-box {
+  background-color: #9ca3af;
+}
+
+.disabled-option {
+  pointer-events: none;
+  opacity: 0.5;
+}
+
+.active {
+  color: burlywood;
+  font-weight: bold;
+}
+</style>
