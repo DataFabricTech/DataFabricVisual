4d7586f737f885ce38c42168d89f0bf4bab9bfd2
leejeongkang <krj1226@gmail.com>
2024-04-09 09:15:52 +0900

    [FAB2023-646] feat: Datepicker Component Composition 1차 개발

---
+++ project/ui/frontend/common/components/extends/datepicker/DatePicker.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <ClientOnly>
+      <DatePicker
+        :clearable="clearable"
+        :confirm="confirm"
+        :confirm-text="confirmText"
+        :disabled="disabled"
+        :disabled-date="disabledDate"
+        :disabled-time="disabledTime"
+        :editable="editable"
+        :format="format"
+        :formatter="formatter"
+        :hour-options="hourOptions"
+        :lang="lang"
+        :minute-options="minuteOptions"
+        :multiple="multiple"
+        :placeholder="placeholder"
+        :range="range"
+        :second-options="secondOptions"
+        :separator="separator"
+        :show-time-panel="dateTimePanel"
+        :title-format="titleFormat"
+        :type="type"
+        :value="modelValue"
+        :value-type="valueType"
+        @update:value="emit('update:modelValue', $event)"
+      >
+        <template v-if="isPanelVisible()" #footer>
+          <button class="mx-btn mx-btn-text" @click="switchDateTimePanel">
+            {{ dateTimePanel ? "select date" : "select time" }}
+          </button>
+        </template>
+      </DatePicker>
+    </ClientOnly>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import DatePicker from "vue-datepicker-next";
+import "vue-datepicker-next/index.css";
+import { DatepickerComposition } from "./DatepickerComposition";
+import { DatepickerProps } from "./DatepickerProps";
+
+const dayjs = useDayjs();
+const props = withDefaults(defineProps<DatepickerProps>(), {
+  modelValue: "",
+  type: "date",
+  range: false,
+  format: "YYYY-MM-DD",
+  valueType: "format",
+  lang: "ko",
+  placeholder: null,
+  editable: true,
+  clearable: true,
+  confirm: false,
+  confirmText: "확인",
+  multiple: false,
+  disabled: false,
+  disabledDateRange: null,
+  disabledTimeRange: null,
+  titleFormat: "YYYY-MM-DD",
+  separator: " ~ ",
+  showTimePanel: false
+});
+const emit = defineEmits(["update:modelValue"]);
+
+const {
+  modelValue,
+  type,
+  range,
+  format,
+  formatter,
+  valueType,
+  lang,
+  placeholder,
+  editable,
+  clearable,
+  confirm,
+  confirmText,
+  multiple,
+  disabled,
+  disabledDateRange,
+  disabledTimeRange,
+  titleFormat,
+  separator,
+  hourOptions,
+  minuteOptions,
+  secondOptions,
+  showTimePanel,
+  dateTimePanel,
+  disabledDate,
+  disabledTime,
+  isPanelVisible,
+  switchDateTimePanel
+} = DatepickerComposition(props);
+</script>
---
+++ project/ui/frontend/common/components/extends/datepicker/DatepickerProps.ts
@@ -0,0 +1,24 @@
+export interface DatepickerProps {
+  modelValue: string[] | string;
+  type?: "date" | "datetime" | "year" | "month" | "time" | "week";
+  range?: boolean;
+  format?: string;
+  formatter?: object;
+  valueType?: string;
+  lang?: string;
+  placeholder?: string | null;
+  editable?: boolean;
+  clearable?: boolean;
+  confirm?: boolean;
+  confirmText?: string;
+  multiple?: boolean;
+  disabled?: boolean;
+  disabledDateRange?: string[] | null;
+  disabledTimeRange?: string[] | null;
+  titleFormat?: string;
+  separator?: string;
+  hourOptions?: number[];
+  minuteOptions?: number[];
+  secondOptions?: number[];
+  showTimePanel?: boolean;
+}
