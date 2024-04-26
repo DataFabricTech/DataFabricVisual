be4f29a1993f86a26f043e0a705054426c246b6a
jungseolhwa <dhqkfskrwl@naver.com>
2024-04-26 17:33:06 +0900

    [FAB2023-547] 디자인가이드페이지 중간저장

---
+++ project/ui/frontend/common/pages/design-guide/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="guide">
+    <guide-lnb></guide-lnb>
+    <div class="guide-container">
+      <main class="guide-main">
+        <router-view></router-view>
+<!--        <guide-button></guide-button>-->
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GuideLnb from "../design-guide/pages/guide-lnb/guide-lnb.vue";
+// import GuideButton from "./pages/button/guide-button.vue"
+
+</script>
+
+<style lang="css" scoped></style>
---
+++ project/ui/frontend/common/pages/design-guide/pages/guide-lnb/guide-lnb.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="guide-lnb">
+    <div class="lnb-top-container">
+      <a class="lnb-logo-link" href="#">
+        <span>Fabric</span>
+        <span>Design</span>
+      </a>
+    </div>
+    <div class="lnb-bottom-container">
+      <ul class="lnb-list">
+        <li class="lnb-item">
+          <p class="lnb-item-title">Foundation</p>
+          <ul class="sub-list">
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">color</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Typography</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Icongraphy</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="lnb-list">
+        <li class="lnb-item">
+          <p class="lnb-item-title">Component</p>
+          <ul class="sub-list">
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Accordion</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Alert</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Badge</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Breadcrumbs</a>
+            </li>
+            <li class="sub-item">
+              <router-link to="/guide-button" class="sub-item-link">Button</router-link>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Button group</a>
+            </li>
+            <li class="sub-item">
+              <a class="sub-item-link" href="#">Checkbox</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "guide-lnb"
+};
+</script>
+
+<style scoped>
+
+</style>
