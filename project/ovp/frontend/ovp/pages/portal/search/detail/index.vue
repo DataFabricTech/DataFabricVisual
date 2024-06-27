ce31eb2fb2c6aadf79b6a1517fd88f7cc1dd240b
soraJang <skysora85@gmail.com>
2024-06-27 16:24:42 +0900

    [FAB2023-898] 탐색 - 상세 resource box 코드 오류 수정 외
    - 파일 위치 변경으로 발생하는 오류 수정
    - 탐색-상세 resource box 테스트 코드 추가

--- project/ovp/frontend/ovp/pages/portal/search/detail/index.vue
+++ project/ovp/frontend/ovp/pages/portal/search/detail/index.vue
@@ -1,4 +1,14 @@
 <template>
+  <resource-box
+    :data-obj="resourceBoxObj"
+    class=""
+    :use-fir-model-nm="true"
+    :show-owner="true"
+    :show-category="true"
+    :use-data-nm-link="false"
+    :show-edit-icon="true"
+    :is-edit-mode="false"
+  />
   <detail-default />
   <Tab
     :data="filteredTabs"
@@ -16,6 +26,7 @@
 import _ from "lodash";
 import { ref, shallowRef } from "vue";
 import Tab from "@extends/tab/Tab.vue";
+import ResourceBox from "@/components/search/resource-box/resource-box.vue";
 import DetailDefault from "@/components/search/detail-default.vue";
 import DefaultInfo from "@/components/search/detail-tab/default-info.vue";
 import Schema from "@/components/search/detail-tab/schema.vue";
@@ -54,6 +65,18 @@ const tabOptions = [
 
 const currentTab = ref(1);
 const currentComponent: Component = shallowRef(DefaultInfo);
+
+let resourceBoxObj: any = {
+  id: "1",
+  serviceIcon: "http://www.mobigen.com/media/img/common/mobigen_logo.svg",
+  depth: ["1depth", "2depth", "3depth", "데이터모델"],
+  firModelNm: "최초 데이터모델 명",
+  modelNm: "Model Name",
+  modelDesc:
+    "데이터 모델 설명에 대한 영역입니다. 데이터 모델 설명에 대한 영역입니다. 데이터 모델 설명에 대한 영역입니다. ",
+  owner: "장소라",
+  category: "카테고리",
+};
 
 function changeTab(item: number | string) {
   currentComponent.value = _.find(tabOptions, ["value", item])?.component;
