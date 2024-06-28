a5700d031881919e96a6e4704172e61ebae67e22
jungseolhwa <dhqkfskrwl@naver.com>
2024-06-28 17:19:35 +0900

    [FAB2023-877] tag-text추가 및 적용, 데이터 상세 퍼블리싱 3차

--- project/ovp/frontend/ovp/pages/_pub/search-detail/index.vue
+++ project/ovp/frontend/ovp/pages/_pub/search-detail/index.vue
@@ -14,7 +14,7 @@
                 <span class="count-text">추천</span>
                 <span class="count-num">5</span>
               </label>
-              <input type="radio" id="button-radio-count-2" class="button-group-input" name="button-group2"/>
+              <input type="radio" id="button-radio-count-2" class="button-group-input" name="button-group2" />
               <label for="button-radio-count-2" class="button-group-label">
                 <svg-icon class="svg-icon" name="thumbs-down"></svg-icon>
                 <span class="count-text">비추천</span>
@@ -22,7 +22,7 @@
               </label>
             </div>
             <div class="checkbox-button checkbox-button-sm">
-              <input type="checkbox" id="checkbox3" class="checkbox-button-input" checked/>
+              <input type="checkbox" id="checkbox3" class="checkbox-button-input" checked />
               <label for="checkbox3" class="checkbox-button-label">
                 <svg-icon class="svg-icon" name="tag"></svg-icon>
                 <span class="count-text">북마크</span>
@@ -53,33 +53,130 @@
                 </div>
               </div>
             </div>
-            <!--  TODO: [개발] 1.<a>였던 resource-box-title 요소를 상세페이지에서는 <span>로 변경 필요
-               2. 수정시 .resource-box-title과 .button-neutral-ghost 상위로 div.flex.gap-2.5가 와야합니다.-->
-            <div class="flex gap-2.5">
-              <span class="resource-box-title">세종특별자치시 상하수도요금표</span>
+            <!--  TODO: [개발] 1.<a>였던 resource-box-title 요소를 상세페이지에서는 <h3>로 변경 필요
+               2. 수정시 .resource-box-요소와 button 상위로 div.resource-box-modify가 와야합니다.-->
+            <div class="resource-box-modify">
+              <h3 class="resource-box-title">세종특별자치시 상하수도요금표</h3>
               <button class="button button-neutral-ghost button-sm" type="button">
                 <span class="hidden-text">수정</span>
                 <svg-icon class="button-icon" name="pen"></svg-icon>
               </button>
             </div>
-            <span class="resource-box-desc">한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다.한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다</span>
+            <div class="resource-box-modify">
+              <lable class="hidden-text" for="title-modify">데이터 모델 이름 입력</lable>
+              <input id="title-modify" class="text-input w-2/4" />
+              <div class="h-group gap-2 shrink-0">
+                <button class="button button-neutral-lighter button-sm" type="button">
+                  취소
+                </button>
+                <button class="button button-primary-lighter button-sm" type="button">
+                  완료
+                </button>
+              </div>
+            </div>
+            <div class="resource-box-modify">
+              <span class="resource-box-desc">한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다.한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다한국교통안전공단에서 교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다교통카드를 이용한 대중교통 사용시  1회 이용요금 평균을 조사한 결과 입니다</span>
+              <button class="button button-neutral-ghost button-sm" type="button">
+                <span class="hidden-text">수정</span>
+                <svg-icon class="button-icon " name="pen"></svg-icon>
+              </button>
+            </div>
+            <div class="resource-box-modify">
+              <lable class="hidden-text" for="description-modify">데이터 모델 설명 입력</lable>
+              <textarea id="description-modify" class="textarea"></textarea>
+              <div class="h-group gap-2">
+                <button class="button button-neutral-lighter button-sm" type="button">
+                  취소
+                </button>
+                <button class="button button-primary-lighter button-sm" type="button">
+                  완료
+                </button>
+              </div>
+            </div>
             <div class="resource-box-info">
-              <dl class="resource-box-list">
-                <dt>소유자</dt>
-                <dd>Owner</dd>
-              </dl>
-              <button class="button button-neutral-ghost button-sm" type="button">
-                <span class="hidden-text">수정</span>
-                <svg-icon class="button-icon" name="pen"></svg-icon>
-              </button>
-              <dl class="resource-box-list">
-                <dt>도메인</dt>
-                <dd>Domain</dd>
-              </dl>
-              <button class="button button-neutral-ghost button-sm" type="button">
-                <span class="hidden-text">수정</span>
-                <svg-icon class="button-icon" name="pen"></svg-icon>
-              </button>
+              <div class="resource-box-modify">
+                <dl class="resource-box-list">
+                  <dt>소유자</dt>
+                  <dd>Owner</dd>
+                </dl>
+                <button class="button button-neutral-ghost button-sm" type="button">
+                  <span class="hidden-text">수정</span>
+                  <svg-icon class="button-icon" name="pen"></svg-icon>
+                </button>
+              </div>
+              <div class="resource-box-modify">
+                <dl class="resource-box-list">
+                  <dt>소유자</dt>
+                  <dd>
+                    <div class="select select-clean is-select-open">
+                      <button class="select-button">
+                        <span class="select-title">Owner</span>
+                        <svg-icon class="svg-icon select-indicator" name="chevron-down-medium"></svg-icon>
+                      </button>
+                      <!-- TODO: [개발] menu-search 시작 컴포넌트화  -->
+                      <div class="menu menu-search">
+                        <div class="menu-head">
+                          <div class="search-input">
+                            <label class="hidden-text" for="text-input-example-11">label</label>
+                            <input id="text-input-example-11" class="text-input" placeholder="검색어 입력" />
+                            <svg-icon class="text-input-icon" name="search"></svg-icon>
+                            <button class="search-input-action-button button button-neutral-ghost button-sm"
+                                    type="button">
+                              <span class="hidden-text">지우기</span>
+                              <svg-icon class="button-icon" name="close"></svg-icon>
+                            </button>
+                          </div>
+                        </div>
+                        <ul class="menu-list">
+                          <li class="menu-item is-menu-item-selected">
+                            <button class="menu-button">
+                              <span class="menu-text">Owner</span>
+                            </button>
+                          </li>
+                          <li class="menu-item">
+                            <button class="menu-button">
+                              <span class="menu-text">Owner22</span>
+                            </button>
+                          </li>
+                          <li class="menu-item">
+                            <button class="menu-button">
+                              <span class="menu-text">root</span>
+                            </button>
+                          </li>
+                          <li class="menu-item">
+                            <button class="menu-button">
+                              <span class="menu-text">honggildong</span>
+                            </button>
+                          </li>
+                          <li class="menu-item">
+                            <button class="menu-button">
+                              <span class="menu-text">mina</span>
+                            </button>
+                          </li>
+                        </ul>
+                        <div class="menu-foot">
+                          <button class="button button-error-ghost">초기화</button>
+                          <div class="menu-foot-group">
+                            <button class="button button-neutral-ghost">취소</button>
+                            <button class="button button-primary">적용</button>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- menu-search 끝 -->
+                    </div>
+                  </dd>
+                </dl>
+              </div>
+              <div class="resource-box-modify">
+                <dl class="resource-box-list">
+                  <dt>도메인</dt>
+                  <dd>Domain</dd>
+                </dl>
+                <button class="button button-neutral-ghost button-sm" type="button">
+                  <span class="hidden-text">수정</span>
+                  <svg-icon class="button-icon" name="pen"></svg-icon>
+                </button>
+              </div>
             </div>
           </div>
           <div class="tab mt-8">
@@ -150,10 +247,10 @@
                     <td>
                       <div class="h-group gap-2.5">
                         <div class="tag tag-primary tag-sm">
-                          <a class="tag-link" href="#">tag</a>
+                          <span class="tag-text">tag</span>
                         </div>
                         <div class="tag tag-primary tag-sm">
-                          <a class="tag-link" href="#">DATA-tag</a>
+                          <span class="tag-text">DATA-tag</span>
                         </div>
                         <button class="button button-neutral-ghost button-sm" type="button">
                           <span class="hidden-text">수정</span>
@@ -167,10 +264,10 @@
                     <td>
                       <div class="h-group gap-2.5">
                         <div class="tag tag-primary tag-sm">
-                          <a class="tag-link" href="#">tag</a>
+                          <span class="tag-text">tag</span>
                         </div>
                         <div class="tag tag-primary tag-sm">
-                          <a class="tag-link" href="#">DATA-tag</a>
+                          <span class="tag-text">DATA-tag</span>
                         </div>
                         <button class="button button-neutral-ghost button-sm" type="button">
                           <span class="hidden-text">수정</span>
@@ -182,10 +279,18 @@
                         <div class="select select-clean">
                           <button class="select-button">
                             <div class="tag tag-primary tag-sm">
-                              <a class="tag-link" href="#">tag</a>
+                              <span class="tag-text">tag</span>
+                              <button class="tag-delete-button">
+                                <span class="hidden-text">삭제</span>
+                                <svg-icon class="svg-icon" name="close"></svg-icon>
+                              </button>
                             </div>
                             <div class="tag tag-primary tag-sm">
-                              <a class="tag-link" href="#">DATA-tag</a>
+                              <span class="tag-text">DATA-tag</span>
+                              <button class="tag-delete-button">
+                                <span class="hidden-text">삭제</span>
+                                <svg-icon class="svg-icon" name="close"></svg-icon>
+                              </button>
                             </div>
                             <svg-icon class="svg-icon select-indicator" name="chevron-down-medium"></svg-icon>
                           </button>
@@ -283,7 +388,7 @@
                   </tbody>
                 </table>
                 <!--  프로파일링 탭  -->
-                <div class="flex flex-col gap-3 h-full">
+                <div class="data-detail-group" style="display: none">
                   <div class="search-input search-input-lg w-96">
                     <label class="hidden-text" for="text-input-example-4">label</label>
                     <input id="text-input-example-4" class="text-input" placeholder="이름으로 검색하세요." />
@@ -293,18 +398,133 @@
                       <svg-icon class="button-icon" name="close"></svg-icon>
                     </button>
                   </div>
-                  <div class="ag-grid" style="width: 100%;height: 100%;border:1px solid #ccc;">
+                  <!--  TODO: [퍼블리싱] ag-grid 개발 후 스타일 적용 예정 -->
+                  <div class="profiling">
                     프로파일링
                   </div>
                 </div>
-                <!--  TODO: [퍼블리싱] 리니지, code-box, ag-grid 개발 후 스타일 적용 예정 -->
+                <!--  TODO: [퍼블리싱] 리니지, code-box,knowledge graph 개발 후 스타일 적용 예정 -->
                 <!--  쿼리 탭 -->
-                <!--  데이터리니지 탭  -->
+                <!--  데이터리니지 탭 시작-->
+                <div class="lineage">
+                  <div class="filters">
+                    <div class="select select-clean">
+                      <button class="select-button">
+                        <span class="select-button-title">카테고리</span>
+                        <svg-icon class="svg-icon select-indicator" name="chevron-down-medium"></svg-icon>
+                      </button>
+                    </div>
+                    <div class="select select-clean">
+                      <button class="select-button">
+                        <span class="select-button-title">소유자</span>
+                        <svg-icon class="svg-icon select-indicator" name="chevron-down-medium"></svg-icon>
+                      </button>
+                    </div>
+                    <div class="select select-clean">
+                      <button class="select-button">
+                        <span class="select-button-title">태그</span>
+                        <svg-icon class="svg-icon select-indicator" name="chevron-down-medium"></svg-icon>
+                      </button>
+                    </div>
+                    <button class="button button-error-lighter button-sm" type="button">
+                      <svg-icon class="button-icon" name="reset"></svg-icon>
+                      <span class="button-title">초기화</span>
+                    </button>
+                  </div>
+                  <div class="preview">
+                    <div class="ml-auto">
+                      <button class="button button-neutral-ghost button-sm">
+                        <svg-icon class="svg-icon" name="close"></svg-icon>
+                        <span class="hidden-text">닫기</span>
+                      </button>
+                    </div>
+                    <div class="preview-contents">
+                      <div class="preview-item">
+                        <a href="#" class="preview-title">
+                          세종특별자치시 상하수도요금표
+                        </a>
+                        <div class="preview-desc">
+                          한국교통안전공단에서 교통카드를 이용한 대중교통 사용시 1회 이용요금 평균을 조사한 결과 입니다.
+                        </div>
+                        <table>
+                          <colgroup>
+                            <col style="width: 30%" />
+                            <col />
+                          </colgroup>
+                          <tr>
+                            <th>확장자</th>
+                            <td>PDF</td>
+                          </tr>
+                          <tr>
+                            <th>전체 행</th>
+                            <td>10000</td>
+                          </tr>
+                        </table>
+                      </div>
+                      <div class="preview-item">
+                        <div class="preview-title">
+                          태그
+                        </div>
+                        <div class="preview-group">
+                          <div class="tag tag-primary tag-sm" v-for="tag in 8" :key="tag">
+                            <span class="tag-text">DATA-tag</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="preview-item">
+                        <div class="preview-title">
+                          용어
+                        </div>
+                        <div class="preview-group">
+                          <div class="tag tag-primary tag-sm">
+                            <span class="tag-text">관련 용어</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="preview-item">
+                        <div class="preview-title">스키마</div>
+                        <div class="v-group gap-2 w-full">
+                          <div class="preview-schema">
+                            <div class="h-group gap-1">
+                              <span class="schema-title">payment_id</span>
+                              <span class="schema-subtitle">(INT)</span>
+                            </div>
+                            <div class="schema-desc">
+                              schema description
+                            </div>
+                          </div>
+                          <div class="preview-schema">
+                            <div class="h-group gap-1">
+                              <span class="schema-title">payment_id</span>
+                              <span class="schema-subtitle">(INT)</span>
+                            </div>
+                            <div class="schema-desc">
+                              schema description
+                            </div>
+                          </div>
+                          <div class="preview-schema">
+                            <div class="h-group gap-1">
+                              <span class="schema-title">payment_id</span>
+                              <span class="schema-subtitle">(INT)</span>
+                            </div>
+                            <div class="schema-desc">
+                              schema description
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="v-group">
+
+                  </div>
+                </div>
+                <!--  데이터리니지 탭 끝-->
                 <!--  knowledge graph  -->
-                <div class="flex gap-4" style="height: 750px;overflow: hidden;display: none">
-                  <div class="knowledge"></div>
-                  <div class="data-detail-list w-2/5">
-                    <div class="resource-box resource-box-short" v-for="card in 6" :key="card">
+                <div class="data-detail-group" style="display: none">
+                  <div class="knowledge">knowledge</div>
+                  <div class="data-detail-list">
+                    <div class="resource-box resource-box-short" v-for="card in 10" :key="card">
                       <div class="resource-box-function">
                         <div class="resource-box-model">
                           <img src="" alt="" />
