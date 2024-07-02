e81ef9b20b58f91cd420de8ab46a72566350cd07
codemotion19 <codemotion@mobigen.com>
2024-07-02 14:27:31 +0900

    [FAB2023-859] 리뷰반영

--- project/ovp/frontend/ovp/pages/_pub/govern/index.vue
+++ project/ovp/frontend/ovp/pages/_pub/govern/index.vue
@@ -6,31 +6,31 @@
       <Sidebar></Sidebar>
       <section id="section">
         <div class="section-contents">
-          <ul class="card-list">
+          <ul class="card">
             <li class="card-item">
               <a href="" title="카테고리">
-                <img src="../../../assets/images/common/ico_category.png" alt="카테고리">
+                <svg-icon name="category" class="svg-icon"></svg-icon>
                 <div class="flex flex-col gap-[16px]">
                   <strong class="card-title">카테고리</strong>
-                  <span class="card-content">다양한 소스에서 메타데이터를 추출하고 연결 설정할 수 있습니다.</span>
+                  <p class="card-desc">다양한 소스에서 메타데이터를 추출하고 연결 설정할 수 있습니다.</p>
                 </div>
               </a>
             </li>
             <li class="card-item">
               <a href="" title="용어사전">
-                <img src="../../../assets/images/common/ico_glossary.png" alt="카테고리">
+                <svg-icon name="glossary" class="svg-icon"></svg-icon>
                 <div class="flex flex-col gap-[16px]">
                   <strong class="card-title">용어사전</strong>
-                  <span class="card-content">사용자와 팀의 관리를 효율적으로 할 수 있습니다.</span>
+                  <p class="card-desc">사용자와 팀의 관리를 효율적으로 할 수 있습니다.</p>
                 </div>
               </a>
             </li>
             <li class="card-item">
               <a href="" title="분류">
-                <img src="../../../assets/images/common/ico_classify.png" alt="카테고리">
+                <svg-icon name="classify" class="svg-icon"></svg-icon>
                 <div class="flex flex-col gap-[16px]">
                   <strong class="card-title">분류</strong>
-                  <span class="card-content">다양한 소스에서 메타데이터를 추출하고 연결 설정할 수 있습니다.</span>
+                  <p class="card-desc">다양한 소스에서 메타데이터를 추출하고 연결 설정할 수 있습니다.</p>
                 </div>
               </a>
             </li>
