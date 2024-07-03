252c04a1bded32058fdf1bc70226aa3bba88fb66
ohmy04180 <ohmy0418@mobigen.com>
2024-07-03 15:32:19 +0900

    [FAB2023-788] feat: 비밀번호 재 설정, 비밀번호 찾기 - 퍼블리싱

--- project/ovp/frontend/ovp/pages/_pub/login/pw-reset/index.vue
+++ project/ovp/frontend/ovp/pages/_pub/login/pw-reset/index.vue
@@ -3,30 +3,61 @@
     <div class="submit-form">
       <h1 class="submit-form-logo">LOGO</h1>
       <div class="submit-form-form">
-        <h2 class="submit-form-title">비밀번호 재설정</h2>
-        <p class="submit-form-desc">
-          비밀번호 재설정 링크를 받으려면 등록된 이메일을 입력하세요
-        </p>
+        <h2 class="submit-form-title">비밀번호 재 설정</h2>
         <div class="form form-lg gap-8">
           <div class="form-body">
             <div class="form-item">
-              <label for="inpEmail" class="form-label">
-                이메일
+              <label for="inpNesPw" class="form-label">
+                신규비밀번호
                 <span class="required">*</span>
               </label>
               <div class="form-detail flex flex-col">
                 <div class="text-input-group w-full">
                   <input
-                    id="inpEmail"
+                    id="inpNesPw"
                     class="text-input text-input-lg"
-                    placeholder="이메일 입력"
+                    placeholder="비밀번호 입력"
                   />
-                  <svg-icon class="text-input-icon" name="user"></svg-icon>
+                  <button
+                    class="text-input-group-action-button button button-neutral-ghost button-sm"
+                    type="button"
+                  >
+                    <span class="hidden-text">지우기</span>
+                    <svg-icon class="button-icon" name="eye"></svg-icon>
+                  </button>
                 </div>
-                <div class="notification notification-sm notification-success">
-                  <svg-icon class="notification-icon" name="success"></svg-icon>
+                <div class="notification notification-sm notification-error">
+                  <svg-icon class="notification-icon" name="error"></svg-icon>
                   <p class="notification-detail">
-                    재설정 링크가 이메일로 전송되었습니다.
+                    사용자 비밀번호를 입력하세요.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="form-item">
+              <label for="inpPw" class="form-label">
+                비밀번호확인
+                <span class="required">*</span>
+              </label>
+              <div class="form-detail flex flex-col">
+                <div class="text-input-group w-full">
+                  <input
+                    id="inpPw"
+                    class="text-input text-input-lg"
+                    placeholder="비밀번호 입력"
+                  />
+                  <button
+                    class="text-input-group-action-button button button-neutral-ghost button-sm"
+                    type="button"
+                  >
+                    <span class="hidden-text">지우기</span>
+                    <svg-icon class="button-icon" name="eye"></svg-icon>
+                  </button>
+                </div>
+                <div class="notification notification-sm notification-error">
+                  <svg-icon class="notification-icon" name="error"></svg-icon>
+                  <p class="notification-detail">
+                    사용자 비밀번호를 입력하세요.
                   </p>
                 </div>
               </div>
@@ -34,7 +65,7 @@
           </div>
           <div class="form-foot">
             <button class="button button-primary button-lg" type="button">
-              제출
+              확인
             </button>
             <button class="button button-primary-ghost button-sm" type="button">
               로그인 페이지로 돌아가기
