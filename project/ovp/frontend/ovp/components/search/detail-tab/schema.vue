30c7af6ff1beff1a415275045d7c09baf32eb53c
leejeongkang <krj1226@gmail.com>
2024-07-04 16:57:49 +0900

    [FAB2023-920] feat: 데이터 모델 상세 - 스키마 탭 개발

--- project/ovp/frontend/ovp/components/search/detail-tab/schema.vue
+++ project/ovp/frontend/ovp/components/search/detail-tab/schema.vue
@@ -1,29 +1,57 @@
 <template>
   <div class="data-detail">
-    <table>
-      <colgroup>
-        <col style="width: 20%" />
-        <col style="width: 20%" />
-        <col style="width: 60%" />
-      </colgroup>
-      <thead>
-        <tr>
-          <th>NAME</th>
-          <th>DATA TYPE</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="tr in 6" :key="tr">
-          <td>요일번호</td>
-          <td>integer</td>
-          <td>no description</td>
-        </tr>
-      </tbody>
-    </table>
+    <agGrid
+      class="ag-theme-alpine ag-theme-quartz"
+      :style="`width: 100%; height: 500px`"
+      :columnDefs="COLUMN_DEFS"
+      :rowData="rowData"
+      rowId="id"
+      :column-width-list="[30, 30, 70]"
+      :setColumnFit="true"
+      :useColumnResize="true"
+    ></agGrid>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import agGrid from "@extends/grid/Grid.vue";
+import type { ColDef } from "ag-grid-community";
+const COLUMN_DEFS: ColDef[] = [
+  { headerName: "NAME", field: "name" },
+  { headerName: "DATA TYPE", field: "data_type" },
+  {
+    headerName: "Description",
+    field: "description",
+    cellRenderer: (params: any) => {
+      return params.value ? params.value : "No Description";
+    },
+  },
+];
+interface schemaType {
+  name: string;
+  data_type: string;
+  description?: string | null;
+}
+const rowData: schemaType[] = [
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+  { name: "Toyota", data_type: "integer" },
+  { name: "Ford", data_type: "integer", description: "description..." },
+  { name: "Porsche", data_type: "integer", description: null },
+];
+</script>
 
 <style lang="scss" scoped></style>
