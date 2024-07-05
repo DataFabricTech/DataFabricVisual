e624adc4e6ae72030a13eef07d112179bed003cd
kimsangwoo66 <mrk24565@naver.com>
2024-07-05 09:19:25 +0900

    [FAB2023-948] feat: 로그인 화면 기본 구조 구현, 로그인단 각 페이지 생성

---
+++ project/ovp/frontend/ovp/pages/portal/login/index.vue
@@ -0,0 +1,9 @@
+<template>
+<submitBox :show-login="true" :show-pw-reset="false"></submitBox>
+</template>
+
+<script setup lang="ts">
+import submitBox from "../../../components/login/submit-box.vue"
+</script>
+
+<style lang="scss" scoped></style>
---
+++ project/ovp/frontend/ovp/pages/portal/login/pw-reset/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <submitBox :show-login="false" :show-pw-reset="true"></submitBox>
+</template>
+<script setup lang="ts">
+import submitBox from "../../../../components/login/submit-box.vue"
+</script>
+<style lang="scss" scoped>
+</style>
