61d02a67c9a9fd9e93ee379f281cef92d55f4099
leejeongkang <krj1226@gmail.com>
2024-07-05 16:28:03 +0900

    [FAB2023-919] feat: 데이터 모델 상세 - 기본 정보 탭 개발

--- project/ovp/frontend/ovp/components/search/detail-tab/default-info.vue
+++ project/ovp/frontend/ovp/components/search/detail-tab/default-info.vue
@@ -7,62 +7,44 @@
       </colgroup>
       <tr>
         <th>유형</th>
-        <td>View</td>
+        <td>{{ modelInfo.type }}</td>
       </tr>
       <tr>
         <th>Columns</th>
-        <td>7</td>
+        <td>{{ modelInfo.columnsLength }}</td>
       </tr>
       <tr>
         <th>Rows</th>
-        <td>105</td>
+        <td>{{ modelInfo.totalCount }}</td>
       </tr>
       <tr>
         <th>태그</th>
         <td>
-          <div class="h-group gap-2.5">
-            <div class="tag tag-primary tag-sm">
-              <span class="tag-text">tag</span>
+          <div class="h-group gap-2.5" v-if="!editTagsMode">
+            <div class="tag tag-primary tag-sm" v-for="tag in modelInfo.tags">
+              <span class="tag-text">{{ tag.label }}</span>
             </div>
-            <div class="tag tag-primary tag-sm">
-              <span class="tag-text">DATA-tag</span>
-            </div>
-            <button class="button button-neutral-ghost button-sm" type="button">
+            <button
+              class="button button-neutral-ghost button-sm"
+              type="button"
+              @click="editTags"
+            >
               <span class="hidden-text">수정</span>
               <svg-icon class="button-icon" name="pen"></svg-icon>
             </button>
           </div>
-        </td>
-      </tr>
-      <tr>
-        <th>용어</th>
-        <td>
-          <div class="h-group gap-2.5">
-            <div class="tag tag-primary tag-sm">
-              <span class="tag-text">tag</span>
-            </div>
-            <div class="tag tag-primary tag-sm">
-              <span class="tag-text">DATA-tag</span>
-            </div>
-            <button class="button button-neutral-ghost button-sm" type="button">
-              <span class="hidden-text">수정</span>
-              <svg-icon class="button-icon" name="pen"></svg-icon>
-            </button>
-          </div>
-          <div class="h-group gap-2.5">
-            <!-- TODO: [개발] 수정버튼 클릭시 select 활성화  -->
+          <div class="h-group gap-2.5" v-if="editTagsMode">
             <div class="select select-clean">
-              <button class="select-button">
-                <div class="tag tag-primary tag-sm">
-                  <span class="tag-text">tag</span>
-                  <button class="tag-delete-button">
-                    <span class="hidden-text">삭제</span>
-                    <svg-icon class="svg-icon" name="close"></svg-icon>
-                  </button>
-                </div>
-                <div class="tag tag-primary tag-sm">
-                  <span class="tag-text">DATA-tag</span>
-                  <button class="tag-delete-button">
+              <button class="select-button" @click.stop="editTags">
+                <div
+                  class="tag tag-primary tag-sm"
+                  v-for="tag in modelInfo.tags"
+                >
+                  <span class="tag-text">{{ tag.label }}</span>
+                  <button
+                    class="tag-delete-button"
+                    @click.stop="deleteTag(tag)"
+                  >
                     <span class="hidden-text">삭제</span>
                     <svg-icon class="svg-icon" name="close"></svg-icon>
                   </button>
@@ -72,99 +54,61 @@
                   name="chevron-down-medium"
                 ></svg-icon>
               </button>
-              <!-- TODO: [개발] menu-search 시작 컴포넌트화  -->
-              <div class="menu menu-search">
-                <div class="menu-head">
-                  <div class="search-input">
-                    <label class="hidden-text" for="text-input-example-11"
-                      >label</label
-                    >
-                    <input
-                      id="text-input-example-11"
-                      class="text-input"
-                      placeholder="검색어 입력"
-                    />
-                    <svg-icon class="text-input-icon" name="search"></svg-icon>
-                    <button
-                      class="search-input-action-button button button-neutral-ghost button-sm"
-                      type="button"
-                    >
-                      <span class="hidden-text">지우기</span>
-                      <svg-icon class="button-icon" name="close"></svg-icon>
-                    </button>
-                  </div>
+              <menu-search
+                :data="tags"
+                :selected-items="modelInfo.tags"
+                :is-multi="true"
+                @multiple-change="changeTags"
+                @cancel="editTags"
+              ></menu-search>
+            </div>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <th>용어</th>
+        <td>
+          <div class="h-group gap-2.5" v-if="!editTermsMode">
+            <div class="tag tag-primary tag-sm" v-for="term in modelInfo.terms">
+              <span class="tag-text">{{ term.label }}</span>
+            </div>
+            <button
+              class="button button-neutral-ghost button-sm"
+              type="button"
+              @click="editTerms"
+            >
+              <span class="hidden-text">수정</span>
+              <svg-icon class="button-icon" name="pen"></svg-icon>
+            </button>
+          </div>
+          <div class="h-group gap-2.5" v-if="editTermsMode">
+            <div class="select select-clean">
+              <button class="select-button" @click.stop="editTerms">
+                <div
+                  class="tag tag-primary tag-sm"
+                  v-for="term in modelInfo.terms"
+                >
+                  <span class="tag-text">{{ term.label }}</span>
+                  <button
+                    class="tag-delete-button"
+                    @click.stop="deleteTerm(term)"
+                  >
+                    <span class="hidden-text">삭제</span>
+                    <svg-icon class="svg-icon" name="close"></svg-icon>
+                  </button>
                 </div>
-                <ul class="menu-list">
-                  <li class="menu-item">
-                    <div class="checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox-menu-1"
-                        class="checkbox-input"
-                      />
-                      <label for="checkbox-menu-1" class="checkbox-label">
-                        Checkbox1
-                      </label>
-                    </div>
-                  </li>
-                  <li class="menu-item">
-                    <div class="checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox-menu-2"
-                        class="checkbox-input"
-                      />
-                      <label for="checkbox-menu-2" class="checkbox-label">
-                        Checkbox2
-                      </label>
-                    </div>
-                  </li>
-                  <li class="menu-item">
-                    <div class="checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox-menu-3"
-                        class="checkbox-input"
-                      />
-                      <label for="checkbox-menu-3" class="checkbox-label">
-                        Checkbox3
-                      </label>
-                    </div>
-                  </li>
-                  <li class="menu-item">
-                    <div class="checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox-menu-4"
-                        class="checkbox-input"
-                      />
-                      <label for="checkbox-menu-4" class="checkbox-label">
-                        Checkbox4
-                      </label>
-                    </div>
-                  </li>
-                  <li class="menu-item">
-                    <div class="checkbox">
-                      <input
-                        type="checkbox"
-                        id="checkbox-menu-5"
-                        class="checkbox-input"
-                      />
-                      <label for="checkbox-menu-5" class="checkbox-label">
-                        Checkbox5
-                      </label>
-                    </div>
-                  </li>
-                </ul>
-                <div class="menu-foot">
-                  <button class="button button-error-ghost">초기화</button>
-                  <div class="menu-foot-group">
-                    <button class="button button-neutral-ghost">취소</button>
-                    <button class="button button-primary">적용</button>
-                  </div>
-                </div>
-              </div>
-              <!-- menu-search 끝 -->
+                <svg-icon
+                  class="svg-icon select-indicator"
+                  name="chevron-down-medium"
+                ></svg-icon>
+              </button>
+              <menu-search
+                :data="terms"
+                :selected-items="modelInfo.terms"
+                :is-multi="true"
+                @multiple-change="changeTerms"
+                @cancel="editTerms"
+              ></menu-search>
             </div>
           </div>
         </td>
@@ -173,6 +117,66 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import menuSearch from "@extends/menu-seach/menu-search.vue";
+import type { MenuSearchItemImpl } from "@extends/menu-seach/MenuSearchComposition";
+const modelInfo = reactive({
+  type: "Regular",
+  columnsLength: 7,
+  totalCount: 10,
+  tags: [
+    { label: "tag 03", value: "tag 03" },
+    { label: "tag 04", value: "tag 04" },
+  ],
+  terms: [
+    { label: "term 01", value: "term 01" },
+    { label: "term 04", value: "term 04" },
+    { label: "term 05", value: "term 05" },
+  ],
+});
+const editTagsMode = ref(false);
+const editTermsMode = ref(false);
+const editTags = () => {
+  editTagsMode.value = !editTagsMode.value;
+};
+
+const editTerms = () => {
+  editTermsMode.value = !editTermsMode.value;
+};
+const deleteTag = (tag: object) => {
+  const index = modelInfo.tags.indexOf(tag);
+  if (index !== -1) {
+    modelInfo.tags.splice(index, 1);
+  }
+};
+const deleteTerm = (term: object) => {
+  const index = modelInfo.terms.indexOf(term);
+  if (index !== -1) {
+    modelInfo.terms.splice(index, 1);
+  }
+};
+const changeTags = (val: MenuSearchItemImpl[]) => {
+  modelInfo.tags = [...val];
+  editTags();
+};
+const changeTerms = (val: MenuSearchItemImpl[]) => {
+  modelInfo.terms = [...val];
+  editTerms();
+};
+const tags = [
+  { label: "tag 01", value: "tag 01" },
+  { label: "tag 02", value: "tag 02" },
+  { label: "tag 03", value: "tag 03" },
+  { label: "tag 04", value: "tag 04" },
+  { label: "tag 05", value: "tag 05" },
+];
+const terms = [
+  { label: "term 01", value: "term 01" },
+  { label: "term 02", value: "term 02" },
+  { label: "term 03", value: "term 03" },
+  { label: "term 04", value: "term 04" },
+  { label: "term 05", value: "term 05" },
+];
+</script>
 
 <style lang="scss" scoped></style>
