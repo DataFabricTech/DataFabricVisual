886f5d6666754bedf822008823ed16883e95c2f5
jungseolhwa <dhqkfskrwl@naver.com>
2024-07-09 18:17:41 +0900

    [FAB2023-798] 메인페이지 퍼블리싱 중간저장

---
+++ project/ovp/frontend/ovp/pages/_pub/main/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="section-contents section-contents-grid" style="flex-wrap: wrap">
+    <div class="grid-content">
+      <div class="l-top-bar">
+        <span>최근 탐색 데이터</span>
+        <button class="button link-button-support">
+          <span class="button-title">모두 보기</span>
+        </button>
+      </div>
+      <div class="v-group">
+        <resource-box></resource-box>
+        <resource-box></resource-box>
+        <resource-box></resource-box>
+      </div>
+    </div>
+    <div class="grid-content">
+      <div class="l-top-bar">
+        <span>북마크 한 데이터</span>
+        <button class="button link-button-support">
+          <span class="button-title">모두 보기</span>
+        </button>
+      </div>
+      <div class="v-group">
+        <resource-box></resource-box>
+
+      </div>
+    </div>
+    </div>
+    <div class="grid-content"></div>
+    <div class="grid-content"></div>
+  </div>
+</template>
+
+<script>
+import ResourceBox from "~/components/common/resource-box/resource-box.vue";
+
+export default {
+  name: "main",
+  components: { ResourceBox }
+};
+</script>
+
+<style scoped>
+</style>
