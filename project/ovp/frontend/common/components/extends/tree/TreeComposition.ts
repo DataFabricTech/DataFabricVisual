7948d59a9c44752eed824be12624052c81139baa
soraJang <skysora85@gmail.com>
2024-07-18 15:10:26 +0900

    [FAB2023-999] chore : 사용하지 않는 fn 삭제

--- project/ovp/frontend/common/components/extends/tree/TreeComposition.ts
+++ project/ovp/frontend/common/components/extends/tree/TreeComposition.ts
@@ -5,8 +5,6 @@ import _ from "lodash";
 export interface TreeComposition extends TreeProps {
   treeItems: Ref<TreeViewItem[]>;
   createNewTreeItem(parentId: string, name?: string, desc?: string, order?: number): TreeViewItem;
-  findNodeById(items: TreeViewItem[], id: string | number): TreeViewItem | null;
-  findParentNode(items: TreeViewItem[], nodeId: string | number): TreeViewItem | null;
   openAll(): void;
   closeAll(): void;
   dropValidatorHandler: any;
@@ -34,71 +32,6 @@ export function TreeComposition(props: TreeProps): TreeComposition {
       children: []
     };
   };
-
-  const findNodeById = (items: TreeViewItem[], id: string | number): TreeViewItem | null => {
-    for (const node of items) {
-      if (node.id === id) {
-        return node;
-      }
-      if (node.children) {
-        const found = findNodeById(node.children, id);
-        if (found) {
-          return found;
-        }
-      }
-    }
-    return null;
-  };
-
-  const findParentNode = (items: TreeViewItem[], nodeId: string | number): TreeViewItem | null => {
-    for (const node of items) {
-      if (node.children) {
-        for (const child of node.children) {
-          if (child.id === nodeId) {
-            return node;
-          }
-        }
-        const parent: TreeViewItem | null = findParentNode(node.children, nodeId);
-        if (parent) {
-          return parent;
-        }
-      }
-    }
-    return null;
-  };
-
-  // NOTE: api 처리를 하지 않고 front 에서 형제 노드 나 자식 노드를 추가 할 경우 아래 코드를 사용한다.
-  // // 형제 노드 추가
-  // const addSibling = (node: TreeViewItem) => {
-  //   const parentNode: TreeViewItem | null = findParentNode(treeItems.value, node.id);
-  //   const newNode = createNewTreeItem(node.parentId);
-  //   if (_.isNull(parentNode)) {
-  //     treeItems.value.push(newNode);
-  //   } else {
-  //     (parentNode.children ??= []).push(newNode);
-  //   }
-  // };
-  // const getDataModelByCtgId = (id: string | number | undefined) => {
-  //   // TODO : 해당 카테고리를 이용하여 생성된 데이터 모델이 있는지 API 조회해야함.
-  //   console.log(`데이터 모델 체크 : ${id}`);
-  //   const getDataModelByCtgId: boolean = false;
-  //   return getDataModelByCtgId;
-  // };
-  // const addChild = (node: TreeViewItem) => {
-  //   const targetNode = findNodeById(treeItems.value, node.id);
-  //
-  //   // step1. validation check : 설정된 데이터 모델이 있는지 조회
-  //   if (getDataModelByCtgId(node.id)) {
-  //     return;
-  //   }
-  //
-  //   // step2 설정된 데이터 모델이 없으면 하위 노드를 추가한다.
-  //   const newNode: TreeViewItem = createNewTreeItem(node.id);
-  //   if (targetNode !== null) {
-  //     (targetNode.children ??= []).push(newNode);
-  //     targetNode.expanded = true;
-  //   }
-  // };
 
   const updateAllNode = (nodes: TreeViewItem[], key: string, bool: boolean) => {
     _.forEach(nodes, (node) => {
@@ -135,8 +68,6 @@ export function TreeComposition(props: TreeProps): TreeComposition {
     ...props,
     treeItems,
     createNewTreeItem,
-    findNodeById,
-    findParentNode,
     openAll,
     closeAll,
     dropValidatorHandler
