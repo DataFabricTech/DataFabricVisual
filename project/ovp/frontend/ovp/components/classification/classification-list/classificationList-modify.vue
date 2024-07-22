d9549fb627a22ae5a79617928bd8fe2ee97a0cd1
김지혜 <kimjihye6245@naver.com>
2024-07-22 16:30:23 +0900

    refactor : 분류명 수정 및 분류설명 -> 기존 동작 EditableGroup컴포넌트사용으로 코드 수정

--- project/ovp/frontend/ovp/components/classification/classification-list/classificationList-modify.vue
+++ project/ovp/frontend/ovp/components/classification/classification-list/classificationList-modify.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="work-page">
     <div class="l-top-bar">
-      <div class="editable-group">
-        <span class="editable-group-title">분류 01</span>
-        <button class="button button-neutral-ghost button-sm" type="button" @click="modifyClas">
-          <span class="hidden-text">수정</span>
-          <svg-icon class="button-icon" name="pen"></svg-icon>
-        </button>
-      </div>
-      <!-- 수정 버튼 클릭시 아래 내용으로 전환됩니다 -->
-      <div class="editable-group" v-if="showModifyClas">
-        <lable class="hidden-text" for="title-modify">분류 이름 수정</lable>
-        <input id="title-modify" class="text-input w-4/5" />
-        <div class="h-group gap-1">
-          <button class="button button-neutral-stroke" type="button" @click="modifyClas">
-            취소
-          </button>
-          <button class="button button-primary-lighter" type="button">
-            완료
-          </button>
-        </div>
+      <div class="v-group gap-[20px]">
+        <editable-group :editable="true" @editCancel="editCancel" @editDone="editDone" @editIcon="editIconClick">
+          <template #edit-slot>
+            <label class="hidden-text" for="title-modify">분류명 입력</label>
+            <input
+                v-model="newData.title"
+                @input="editInput($event)"
+                placeholder="분류명에 대한 영역입니다."
+                required
+                id="title-modify"
+                class="text-input"
+            />
+          </template>
+          <template #view-slot>
+            <h3 class="editable-group-title">{{ newData.title }}</h3>
+          </template>
+        </editable-group>
       </div>
       <button class="button button-error-lighter">삭제</button>
     </div>
@@ -31,38 +29,24 @@
           <p class="notification-detail">등록된 정보가 없습니다.</p>
         </div>
       </div>
-      <div class="editable-group">
-            <span class="editable-group-desc"
-            >GDPR special category data is personal information of data
-              subjects that is especially sensitive, the exposure of which could
-              significantly impact the rights and freedoms of data subjects and
-              potentially be used against them for unlawful
-              discrimination.</span
-            >
-        <button class="button button-neutral-ghost button-sm" type="button" @click="modifyClasDescription">
-          <span class="hidden-text">수정</span>
-          <svg-icon class="button-icon" name="pen"></svg-icon>
-        </button>
-      </div>
-      <!-- 수정 버튼 클릭시 아래 내용으로 전환됩니다 -->
-      <div class="editable-group" v-if="showModifyClasDescription">
-        <lable class="hidden-text" for="description-modify"
-        >분류 설명 수정
-        </lable>
-        <textarea
-            id="description-modify"
-            class="textarea"
-            width="300px"
-        ></textarea>
-        <div class="h-group gap-1">
-          <button class="button button-neutral-stroke" type="button" @click="modifyClasDescription">
-            취소
-          </button>
-          <button class="button button-primary-lighter" type="button">
-            완료
-          </button>
-        </div>
-      </div>
+      <!-- 수정 버튼 클릭시 아래 내용으로 전환 -->
+      <editable-group :editable="true" @editCancel="editCancel" @editDone="editDone" @editIcon="editIconClick">
+        <template #edit-slot>
+          <label class="hidden-text" for="textarea-modify">textarea 입력</label>
+          <textarea
+              class="textarea"
+              v-model="newData.description"
+              @input="editInput($event)"
+              placeholder="해당 분류 설명에 대한 영역입니다."
+              required
+              id="textarea-modify"
+          ></textarea>
+        </template>
+        <template #view-slot>
+          <p class="editable-group-desc">{{ newData.description }}</p>
+        </template>
+      </editable-group>
+      <!-- 해당 분류의 태그 리스트 영역-->
       <div>
         <tag-list></tag-list>
       </div>
@@ -70,17 +54,48 @@
   </div>
 </template>
 <script setup lang="ts">
-import tagList from "./tagList.vue"
+import tagList from "@/components/classification/classification-list/tagList.vue"
+import EditableGroup from "@extends/editable-group/EditableGroup.vue";
+import _ from "lodash";
 
-const showModifyClas = ref(false);
-const showModifyClasDescription = ref(false);
+// 받아올 API 데이터. 완료를 누르자마자 새로 받아와야 함.
+const defaultData = ref<Record<string, any>>({
+  title: "분류1",
+  description:
+      "GDPR special category data is personal information of data subjects that is especially sensitive, the exposure of which could significantly impact the rights and freedoms of data subjects and potentially be used against them for unlawful discrimination.",
+  check: false
+});
 
-const modifyClas = () => {
-  showModifyClas.value = !showModifyClas.value;
-}
-const modifyClasDescription = () => {
-  showModifyClasDescription.value = !showModifyClasDescription .value;
-}
+// 변경될 데이터
+const newData = ref<Record<string, any>>(_.cloneDeep(defaultData.value));
+
+const editInput = (event: Event) => {
+  // Input event
+  const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+  console.log(`event: ${target.value}`);
+};
+
+const editIconClick = () => {
+  // Button Click event
+  console.log(`icon event`);
+};
+
+const editCancel = () => {
+  // Cancel event
+  newData.value = defaultData.value;
+  console.log(`cancel event`);
+};
+
+const editDone = () => {
+  // Done event
+  console.log(`done event`);
+
+  defaultData.value = _.cloneDeep(newData.value);
+
+  // TODO: [개발] 적용/완료 버튼 클릭 시 API 전달 필요
+  console.log("defaultData", defaultData.value);
+  console.log("newData: ", newData.value);
+};
 </script>
 
 <style lang="scss" scoped></style>
