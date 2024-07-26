251debd42875c902a52141827b30e3a830463ab8
soraJang <skysora85@gmail.com>
2024-07-26 11:21:23 +0900

    [FAB2023-898] chore : 오타 수정

--- project/ovp/frontend/ovp/store/search/common.ts
+++ project/ovp/frontend/ovp/store/search/common.ts
@@ -118,11 +118,11 @@ export const useSearchCommonStore = defineStore("searchCommon", () => {
       query_filter: JSON.stringify(queryFilter),
       sort_field: sortKey.value,
       sort_order: sortKeyOpt.value,
-      trino_query: JSON.stringify(getTorinoQuery(queryFilter)),
+      trino_query: JSON.stringify(getTrinoQuery(queryFilter)),
     };
     return new URLSearchParams(params);
   };
-  const getTorinoQuery = (queryFilter: QueryFilter) => {
+  const getTrinoQuery = (queryFilter: QueryFilter) => {
     // query 구현을 backend 에서 하려니까 코드가 너무 복잡해져서 front 에 해서 넘겨서 처리.
     const trinoFilter: QueryFilter = {
       query: {
