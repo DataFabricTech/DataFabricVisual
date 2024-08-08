772d79a28cedcd5b10dd2ab8f07e66a0747e2264
soraJang <skysora85@gmail.com>
2024-08-08 09:29:21 +0900

    [FAB2023-999] feat: patch 동작 제대로 안됨 수정 외
    - category update 기능 동작 오류 수정
    - modelList 조회하는 부분 tagId 로 조회하도록 수정

--- project/ovp/frontend/ovp/store/governance/Category/index.ts
+++ project/ovp/frontend/ovp/store/governance/Category/index.ts
@@ -33,24 +33,24 @@ export const useGovernCategoryStore = defineStore("GovernCategory", () => {
     categories.value = data.children;
     isCategoriesNoData.value = categories.value.length === 0;
   };
-  const getModelListQuery = (id: string) => {
+  const getModelListQuery = (tagId: string) => {
     // TODO : [개발] 검색어 조건 여기 추가.
     const params: any = {
       // eslint-disable-next-line id-length
       q: "",
       from: from.value,
       size: size.value,
-      categoryId: id,
+      tagId: tagId,
     };
     return new URLSearchParams(params);
   };
 
   const getModelByCategoryIdAPI = async (node: TreeViewItem) => {
-    if (_.isNull(node) || _.isEmpty(node.id)) {
+    if (_.isNull(node) || _.isEmpty(node.tagId)) {
       return;
     }
     const { data } = await $api(
-      `/api/category/models?${getModelListQuery(node.id)}`,
+      `/api/category/models?${getModelListQuery(node.tagId)}`,
     );
     return data;
   };
@@ -77,8 +77,6 @@ export const useGovernCategoryStore = defineStore("GovernCategory", () => {
     insertOrEditAPI("PATCH", node);
   };
   const insertOrEditAPI = (method: string, node: TreeViewItem) => {
-    // TODO : [개발] edit의 경우에도 id, parentId, name, desc, order 값을 모두 backend 로 보내야 함. 없는 항목을 'null'로 업데이트 됨.
-    // TODO: [개발] 정상적으로 PATCH 동작이 되지 않음. API 검토 요청
     $api("/api/category", {
       method: method,
       body: node,
--- project/ovp/server/src/main/java/com/mobigen/ovp/search/SearchService.java
+++ project/ovp/server/src/main/java/com/mobigen/ovp/search/SearchService.java
@@ -129,9 +129,6 @@ public class SearchService {
     }
 
     private Map<String, Object> getAllList(MultiValueMap<String, String> params) throws Exception {
-//        if (!params.getFirst("index").equals("all")) {
-//            params.set("size", "0");
-//        }
         params.set("index", ModelIndex.all.name());
         return getList(params);
     }
