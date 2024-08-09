75646d0b907d56cf5dbef46cef89f155e46dc83d
jungseolhwa <dhqkfskrwl@naver.com>
2024-08-09 16:05:04 +0900

    [FAB2023-1121] 프롭스 수정

--- project/ovp/frontend/common/components/extends/menu-seach/tree/MenuSearchTreeProps.ts
+++ project/ovp/frontend/common/components/extends/menu-seach/tree/MenuSearchTreeProps.ts
@@ -1,5 +1,4 @@
-import { MenuSearchTreeContentsProps } from "~/components/extends/menu-seach/tree/contents/MenuSearchTreeContentsProps";
-
+import { MenuSearchTreeContentsProps } from "./contents/MenuSearchTreeContentsProps";
 export interface MenuSearchTreeProps extends MenuSearchTreeContentsProps {
   title?: string;
 }
