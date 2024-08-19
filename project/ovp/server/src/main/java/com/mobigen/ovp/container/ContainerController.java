2382c1668439d6876b93e396d47c3c488055b8c3
ohmy04180 <ohmy0418@mobigen.com>
2024-08-19 11:47:53 +0900

    [FAB2023-898] refactor: 미리보기 - api 동일한 형태로 수정

--- project/ovp/server/src/main/java/com/mobigen/ovp/container/ContainerController.java
+++ project/ovp/server/src/main/java/com/mobigen/ovp/container/ContainerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
-@RequestMapping("api/containers")
+@RequestMapping("/api/containers")
 @RestController
 @RequiredArgsConstructor
 public class ContainerController {
