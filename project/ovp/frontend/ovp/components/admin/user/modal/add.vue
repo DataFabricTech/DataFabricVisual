758bbe7a9ad673615f008047010f48eaa99eb8c3
ygkim97 <dnjdn21@naver.com>
2024-08-20 16:17:12 +0900

    [FAB2023-1193] feat: 사용자 관리 메인페이지 - 사용자 추가 모달 컴포넌트 분리

---
+++ project/ovp/frontend/ovp/components/admin/user/modal/add.vue
@@ -0,0 +1,231 @@
+<template>
+  <Modal
+    modal-id="addUserModal"
+    title="사용자 추가"
+    :click-to-close="true"
+    :esc-to-close="true"
+    :display-directive="'show'"
+    :width="480"
+    :height="516"
+    confirmBtnMsg="생성"
+  >
+    <template v-slot:body>
+      <div class="form form-vertical">
+        <div class="form-body">
+          <div class="form-item">
+            <label for="" class="form-label">
+              이메일
+              <span class="required">*</span>
+            </label>
+            <div class="form-detail">
+              <div class="search-input">
+                <label class="hidden-text" for="service-add-input-email"
+                  >label</label
+                >
+                <input
+                  id="service-add-input-email"
+                  class="text-input text-input-lg"
+                  placeholder="이메일"
+                />
+                <button
+                  class="search-input-action-button button button-neutral-ghost button-sm"
+                  type="button"
+                >
+                  <span class="hidden-text">지우기</span>
+                  <svg-icon class="button-icon" name="close"></svg-icon>
+                </button>
+              </div>
+              <div class="notification notification-sm notification-error">
+                <svg-icon class="notification-icon" name="error"></svg-icon>
+                <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-item">
+            <label for="" class="form-label"> 이름표시 </label>
+            <div class="form-detail flex flex-col">
+              <div class="search-input">
+                <label class="hidden-text" for="service-add-input-name"
+                  >label</label
+                >
+                <input
+                  id="service-add-input-name"
+                  class="text-input text-input-lg"
+                  placeholder="이름"
+                />
+                <button
+                  class="search-input-action-button button button-neutral-ghost button-sm"
+                  type="button"
+                >
+                  <span class="hidden-text">지우기</span>
+                  <svg-icon class="button-icon" name="close"></svg-icon>
+                </button>
+              </div>
+              <div class="notification notification-sm notification-error">
+                <svg-icon class="notification-icon" name="error"></svg-icon>
+                <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-item">
+            <label class="form-label" for="service-add-description">
+              설명
+            </label>
+            <div class="form-detail">
+              <textarea
+                id="service-add-description"
+                class="textarea h-28"
+                placeholder="설명을 입력하세요."
+              ></textarea>
+              <div class="notification notification-sm notification-error">
+                <svg-icon class="notification-icon" name="error"></svg-icon>
+                <p class="notification-detail">설명을 입력하세요.</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-item">
+            <label for="" class="form-label">비밀번호</label>
+            <div class="form-detail gap-2">
+              <div class="radio-group">
+                <div class="radio">
+                  <input
+                    type="radio"
+                    id="radiosm"
+                    class="radio-input"
+                    name="radio2"
+                    checked
+                  />
+                  <label for="radiosm" class="radio-label">
+                    비밀번호 자동생성
+                  </label>
+                </div>
+                <div class="radio">
+                  <input
+                    type="radio"
+                    id="radiosm3"
+                    class="radio-input"
+                    name="radio2"
+                  />
+                  <label for="radiosm3" class="radio-label"
+                    >비밀번호 직접생성
+                  </label>
+                </div>
+              </div>
+              <!-- 비밀번호 자동생성 -->
+              <div class="text-input-group">
+                <div class="v-group w-full">
+                  <div class="search-input">
+                    <label class="hidden-text" for="service-add-input-password"
+                      >label</label
+                    >
+                    <input
+                      id="service-add-input-password"
+                      type="password"
+                      class="text-input text-input-lg"
+                      value="prefix icon + button"
+                    />
+                    <button
+                      class="search-input-action-button button button-neutral-ghost button-sm"
+                      type="button"
+                    >
+                      <span class="hidden-text">지우기</span>
+                      <svg-icon class="button-icon" name="eye-hide"></svg-icon>
+                    </button>
+                  </div>
+                  <div class="notification notification-sm notification-error">
+                    <svg-icon class="notification-icon" name="error"></svg-icon>
+                    <p class="notification-detail">
+                      얼럿 메세지를 입력해주세요.
+                    </p>
+                  </div>
+                </div>
+                <button class="button button-neutral-stroke button-xl">
+                  <span class="hidden-text"></span>
+                  <svg-icon class="icons button-icon" name="reset"></svg-icon>
+                </button>
+                <button class="button button-neutral-stroke button-xl">
+                  <span class="hidden-text"></span>
+                  <svg-icon class="icons button-icon" name="copy"></svg-icon>
+                </button>
+              </div>
+              <!-- 비밀번호 직접생성 -->
+              <div class="search-input">
+                <label
+                  class="hidden-text"
+                  for="service-add-input-password-direct"
+                  >label</label
+                >
+                <input
+                  id="service-add-input-password-direct"
+                  type="password"
+                  class="text-input text-input-lg"
+                  value="prefix icon + button"
+                />
+                <button
+                  class="search-input-action-button button button-neutral-ghost button-sm"
+                  type="button"
+                >
+                  <span class="hidden-text">지우기</span>
+                  <svg-icon class="button-icon" name="eye-hide"></svg-icon>
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- 비밀번호 직접생성 선택시 노출 -->
+          <div class="form-item">
+            <label for="" class="form-label">비밀번호 확인</label>
+            <div class="form-detail">
+              <div class="search-input">
+                <label
+                  class="hidden-text"
+                  for="service-add-input-password-check"
+                  >label</label
+                >
+                <input
+                  id="service-add-input-password-check"
+                  type="password"
+                  class="text-input text-input-lg"
+                  value="prefix icon + button"
+                />
+                <button
+                  class="search-input-action-button button button-neutral-ghost button-sm"
+                  type="button"
+                >
+                  <span class="hidden-text">지우기</span>
+                  <svg-icon class="button-icon" name="eye-hide"></svg-icon>
+                </button>
+              </div>
+              <div class="notification notification-sm notification-error">
+                <svg-icon class="notification-icon" name="error"></svg-icon>
+                <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
+              </div>
+            </div>
+          </div>
+          <div class="form-item">
+            <label for="" class="form-label">관리자</label>
+            <div class="form-detail">
+              <div class="switch">
+                <input type="checkbox" id="manager-sw" class="switch-input" />
+                <label for="manager-sw" class="switch-label">
+                  <div class="switch-control"></div>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from "@extends/modal/Modal.vue";
+</script>
+
+<style>
+/* TODO: Modal 컴포넌트 사용하려면 해당 style 필요, 퍼블리싱 요청 */
+.modal-body {
+  height: 400px;
+  overflow: auto;
+}
+</style>
--- project/ovp/frontend/ovp/pages/portal/admin/user/index.vue
+++ project/ovp/frontend/ovp/pages/portal/admin/user/index.vue
@@ -11,7 +11,7 @@
         @onClickSearch="onClickSearch"
         :placeholder="'검색어를 입력하세요.'"
       ></SearchInput>
-      <button class="button button-secondary ml-auto" @click="showModal = true">
+      <button class="button button-secondary ml-auto" @click="openModal">
         사용자 추가
       </button>
     </div>
@@ -57,267 +57,25 @@
       </div>
     </div>
   </div>
-  <!--  TODO: Modal 카테고리 추가  width:480px height:516px-->
-  <div class="modal-fixed vfm--fixed vfm--inset" v-if="showModal">
-    <div class="modal modal-padding-16" style="width: 480px">
-      <div class="modal-head">
-        <div class="modal-head-text">
-          <span class="modal-head-title">서비스 추가</span>
-        </div>
-        <button
-          class="button link-button button-sm"
-          type="button"
-          @click="showModal = false"
-        >
-          <span class="hidden-text">닫기</span>
-          <svg-icon class="button-icon" name="close"></svg-icon>
-        </button>
-      </div>
-      <div class="modal-body" style="height: 400px; overflow: auto">
-        <div class="form form-vertical">
-          <div class="form-body">
-            <div class="form-item">
-              <label for="" class="form-label">
-                이메일
-                <span class="required">*</span>
-              </label>
-              <div class="form-detail">
-                <div class="search-input">
-                  <label class="hidden-text" for="service-add-input-email"
-                    >label</label
-                  >
-                  <input
-                    id="service-add-input-email"
-                    class="text-input text-input-lg"
-                    placeholder="이메일"
-                  />
-                  <button
-                    class="search-input-action-button button button-neutral-ghost button-sm"
-                    type="button"
-                  >
-                    <span class="hidden-text">지우기</span>
-                    <svg-icon class="button-icon" name="close"></svg-icon>
-                  </button>
-                </div>
-                <div class="notification notification-sm notification-error">
-                  <svg-icon class="notification-icon" name="error"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-            </div>
-            <div class="form-item">
-              <label for="" class="form-label"> 이름표시 </label>
-              <div class="form-detail flex flex-col">
-                <div class="search-input">
-                  <label class="hidden-text" for="service-add-input-name"
-                    >label</label
-                  >
-                  <input
-                    id="service-add-input-name"
-                    class="text-input text-input-lg"
-                    placeholder="이름"
-                  />
-                  <button
-                    class="search-input-action-button button button-neutral-ghost button-sm"
-                    type="button"
-                  >
-                    <span class="hidden-text">지우기</span>
-                    <svg-icon class="button-icon" name="close"></svg-icon>
-                  </button>
-                </div>
-                <div class="notification notification-sm notification-error">
-                  <svg-icon class="notification-icon" name="error"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-            </div>
-            <div class="form-item">
-              <label class="form-label" for="service-add-description">
-                설명
-              </label>
-              <div class="form-detail">
-                <textarea
-                  id="service-add-description"
-                  class="textarea h-28"
-                  placeholder="설명을 입력하세요."
-                ></textarea>
-                <div class="notification notification-sm notification-error">
-                  <svg-icon class="notification-icon" name="error"></svg-icon>
-                  <p class="notification-detail">설명을 입력하세요.</p>
-                </div>
-              </div>
-            </div>
-            <div class="form-item">
-              <label for="" class="form-label">비밀번호</label>
-              <div class="form-detail gap-2">
-                <div class="radio-group">
-                  <div class="radio">
-                    <input
-                      type="radio"
-                      id="radiosm"
-                      class="radio-input"
-                      name="radio2"
-                      checked
-                    />
-                    <label for="radiosm" class="radio-label">
-                      비밀번호 자동생성
-                    </label>
-                  </div>
-                  <div class="radio">
-                    <input
-                      type="radio"
-                      id="radiosm3"
-                      class="radio-input"
-                      name="radio2"
-                    />
-                    <label for="radiosm3" class="radio-label"
-                      >비밀번호 직접생성
-                    </label>
-                  </div>
-                </div>
-                <!-- 비밀번호 자동생성 -->
-                <div class="text-input-group">
-                  <div class="v-group w-full">
-                    <div class="search-input">
-                      <label
-                        class="hidden-text"
-                        for="service-add-input-password"
-                        >label</label
-                      >
-                      <input
-                        id="service-add-input-password"
-                        type="password"
-                        class="text-input text-input-lg"
-                        value="prefix icon + button"
-                      />
-                      <button
-                        class="search-input-action-button button button-neutral-ghost button-sm"
-                        type="button"
-                      >
-                        <span class="hidden-text">지우기</span>
-                        <svg-icon
-                          class="button-icon"
-                          name="eye-hide"
-                        ></svg-icon>
-                      </button>
-                    </div>
-                    <div
-                      class="notification notification-sm notification-error"
-                    >
-                      <svg-icon
-                        class="notification-icon"
-                        name="error"
-                      ></svg-icon>
-                      <p class="notification-detail">
-                        얼럿 메세지를 입력해주세요.
-                      </p>
-                    </div>
-                  </div>
-                  <button class="button button-neutral-stroke button-xl">
-                    <span class="hidden-text"></span>
-                    <svg-icon class="icons button-icon" name="reset"></svg-icon>
-                  </button>
-                  <button class="button button-neutral-stroke button-xl">
-                    <span class="hidden-text"></span>
-                    <svg-icon class="icons button-icon" name="copy"></svg-icon>
-                  </button>
-                </div>
-                <!-- 비밀번호 직접생성 -->
-                <div class="search-input">
-                  <label
-                    class="hidden-text"
-                    for="service-add-input-password-direct"
-                    >label</label
-                  >
-                  <input
-                    id="service-add-input-password-direct"
-                    type="password"
-                    class="text-input text-input-lg"
-                    value="prefix icon + button"
-                  />
-                  <button
-                    class="search-input-action-button button button-neutral-ghost button-sm"
-                    type="button"
-                  >
-                    <span class="hidden-text">지우기</span>
-                    <svg-icon class="button-icon" name="eye-hide"></svg-icon>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <!-- 비밀번호 직접생성 선택시 노출 -->
-            <div class="form-item">
-              <label for="" class="form-label">비밀번호 확인</label>
-              <div class="form-detail">
-                <div class="search-input">
-                  <label
-                    class="hidden-text"
-                    for="service-add-input-password-check"
-                    >label</label
-                  >
-                  <input
-                    id="service-add-input-password-check"
-                    type="password"
-                    class="text-input text-input-lg"
-                    value="prefix icon + button"
-                  />
-                  <button
-                    class="search-input-action-button button button-neutral-ghost button-sm"
-                    type="button"
-                  >
-                    <span class="hidden-text">지우기</span>
-                    <svg-icon class="button-icon" name="eye-hide"></svg-icon>
-                  </button>
-                </div>
-                <div class="notification notification-sm notification-error">
-                  <svg-icon class="notification-icon" name="error"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-            </div>
-            <div class="form-item">
-              <label for="" class="form-label">관리자</label>
-              <div class="form-detail">
-                <div class="switch">
-                  <input type="checkbox" id="manager-sw" class="switch-input" />
-                  <label for="manager-sw" class="switch-label">
-                    <div class="switch-control"></div>
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-foot">
-        <div class="modal-foot-group">
-          <button
-            class="button button-neutral-ghost button-lg"
-            @click="showModal = false"
-          >
-            취소
-          </button>
-          <button class="button button-primary button-lg">생성</button>
-        </div>
-      </div>
-    </div>
-  </div>
+
+  <AddUserModal></AddUserModal>
 </template>
 
 <script setup lang="ts">
+import { useNuxtApp } from "nuxt/app";
 import { useRouter } from "#vue-router";
 import { useUserStore } from "@/store/user/userStore";
 import { useIntersectionObserver } from "~/composables/intersectionObserverHelper";
 import SearchInput from "@extends/search-input/SearchInput.vue";
 import agGrid from "@extends/grid/Grid.vue";
 import Loading from "@base/loading/Loading.vue";
+import AddUserModal from "@/components/admin/user/modal/add.vue";
 
+const { $vfm } = useNuxtApp();
 const router = useRouter();
 const userStore = useUserStore();
 const { setSearchKeyword, getUserList, addUserList, deleteUser } = userStore;
 const { userList } = storeToRefs(userStore);
-
-const showModal = ref(false);
 
 const columnDefs = ref([
   {
@@ -355,6 +113,10 @@ const columnDefs = ref([
 const onClickSearch = (value: string) => {
   setSearchKeyword(value);
   getUserList();
+};
+
+const openModal = () => {
+  $vfm.open("addUserModal");
 };
 
 const cellClicked = ({
