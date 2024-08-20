69e5fb014a2000b77771456017a2cd7f5c22763d
jungseolhwa <dhqkfskrwl@naver.com>
2024-08-20 17:30:17 +0900

    [FAB2023-681] 툴팁 퍼블리싱 중간저장

--- project/ovp/frontend/common/pages/design-guide/pages/guide-tooltip.vue
+++ project/ovp/frontend/common/pages/design-guide/pages/guide-tooltip.vue
@@ -2,26 +2,26 @@
   <section class="guide-section">
     <p class="section-type">Component</p>
     <h2 class="section-title">tooltip</h2>
-    <p class="section-summary">툴팁 설명</p>
+    <p class="section-summary">툴팁은 사용자가 요소에 마우스를 올리거나 초점을 맞췄을 때 나타나는 짧은 설명이나 정보를 제공하는 작은 팝업 컴포넌트입니다.</p>
     <article class="mb-10">
       <h3 class="section-subtitle">Tooltip</h3>
       <div class="flex flex-col">
         <div class="flex gap-8 mb-14">
           <div class="v-group gap-4">
-            <div v-tooltip:top.secondary="`Icon Tooltip Sample!`">
+            <div class="tooltip" v-tooltip:top.secondary="`Icon Tooltip Sample!`">
               <svg-icon class="svg-icon" name="info" style="color: #005acd"></svg-icon>
             </div>
-            <p v-tooltip="`Tooltip Sample`">Tooltip Sample</p>
-            <p v-tooltip:top="`My name is ${name}`">Tooltip Sample - Top</p>
-            <p
+            <p class="tooltip" v-tooltip="`Tooltip Sample`">Tooltip Sample</p>
+            <p class="tooltip" v-tooltip:top="`My name is ${name}`">Tooltip Sample - Top</p>
+            <p class="tooltip"
               v-tooltip:bottom.secondary="
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, voluptatem. '
               "
             >
               Tooltip Sample - Secondary
             </p>
-            <p v-tooltip:left.accent="`Tooltip Accent?`">Tooltip Sample - Accent</p>
-            <p v-tooltip:right.primary="`Tooltip Primary`">Tooltip Sample - Primary</p>
+            <p class="tooltip" v-tooltip:left.accent="`Tooltip Accent?`">Tooltip Sample - Accent</p>
+            <p class="tooltip" v-tooltip:right.primary="`Tooltip Primary`">Tooltip Sample - Primary</p>
             <tooltip position="bottom">
               <template v-slot:text>
                 <p>Point at me and find out the secret</p>
