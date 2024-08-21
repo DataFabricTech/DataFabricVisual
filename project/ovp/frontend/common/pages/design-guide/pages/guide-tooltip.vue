a932717370ef5c08fc1a8b5bbe3b91949f2d0941
jungseolhwa <dhqkfskrwl@naver.com>
2024-08-21 10:22:04 +0900

    [FAB2023-681] 툴팁 퍼블리싱 마무리

--- project/ovp/frontend/common/pages/design-guide/pages/guide-tooltip.vue
+++ project/ovp/frontend/common/pages/design-guide/pages/guide-tooltip.vue
@@ -4,32 +4,67 @@
     <h2 class="section-title">tooltip</h2>
     <p class="section-summary">툴팁은 사용자가 요소에 마우스를 올리거나 초점을 맞췄을 때 나타나는 짧은 설명이나 정보를 제공하는 작은 팝업 컴포넌트입니다.</p>
     <article class="mb-10">
-      <h3 class="section-subtitle">Tooltip</h3>
+      <h3 class="section-subtitle">Primary</h3>
       <div class="flex flex-col">
         <div class="flex gap-8 mb-14">
-          <div class="v-group gap-4">
-            <div class="tooltip" v-tooltip:top.secondary="`Icon Tooltip Sample!`">
-              <svg-icon class="svg-icon" name="info" style="color: #005acd"></svg-icon>
+          <figure class="section-figure">
+            <div class="flex justify-center gap-6">
+              <div class="tooltip" v-tooltip:top="`top tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip" v-tooltip:right="`right tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip" v-tooltip:bottom="`bottom tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip" v-tooltip:left="`left tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
             </div>
-            <p class="tooltip" v-tooltip="`Tooltip Sample`">Tooltip Sample</p>
-            <p class="tooltip" v-tooltip:top="`My name is ${name}`">Tooltip Sample - Top</p>
-            <p class="tooltip"
-              v-tooltip:bottom.secondary="
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, voluptatem. '
-              "
-            >
-              Tooltip Sample - Secondary
+            <div class="flex justify-center gap-6">
+              <div class="tooltip" v-tooltip:top="`top tooltip`">
+                <p>Text Hover</p>
+              </div>
+            </div>
+          </figure>
+          <div class="group-description">
+            <p class="group-summary">
+              Primary tooltip은 디폴트 스타일입니다. tooltip은 기본 top, right, bottom, left에서 나타나며 아이콘 또는 텍스트에 hover시 나타날 수 있습니다.
             </p>
-            <p class="tooltip" v-tooltip:left.accent="`Tooltip Accent?`">Tooltip Sample - Accent</p>
-            <p class="tooltip" v-tooltip:right.primary="`Tooltip Primary`">Tooltip Sample - Primary</p>
-            <tooltip position="bottom">
-              <template v-slot:text>
-                <p>Point at me and find out the secret</p>
-              </template>
-              <template v-slot:tooltip>
-                <strong>Watermelon is a berry</strong>
-              </template>
-            </tooltip>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article class="mb-10">
+      <h3 class="section-subtitle">Light</h3>
+      <div class="flex flex-col">
+        <div class="flex gap-8 mb-14">
+          <figure class="section-figure">
+            <div class="flex justify-center gap-6">
+              <div class="tooltip tooltip-light" v-tooltip:top="`top tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip tooltip-light" v-tooltip:right="`right tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip tooltip-light" v-tooltip:bottom="`bottom tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+              <div class="tooltip tooltip-light" v-tooltip:left="`left tooltip`">
+                <svg-icon class="svg-icon" name="help-outline"></svg-icon>
+              </div>
+            </div>
+            <div class="flex justify-center gap-6">
+              <div class="tooltip tooltip-light" v-tooltip:top="`top tooltip`">
+                <p>Text Hover</p>
+              </div>
+            </div>
+          </figure>
+          <div class="group-description">
+            <p class="group-summary">
+              밝은 색상의 light tooltip입니다.
+            </p>
           </div>
         </div>
       </div>
