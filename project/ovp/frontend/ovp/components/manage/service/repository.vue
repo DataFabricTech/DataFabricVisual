21bcbf2208d8c2156b7bd4d69e0a74310c3256f8
김지혜 <kimjihye6245@naver.com>
2024-08-21 10:56:04 +0900

    "[FAB2023-1177]feat: 서비스관리 페이지 - 저장소 탭 > 설명 editable-group 및 스토어 작업 & 서비스 목록 클릭시 설명 API호출 / 스토어 - set함수 추가"

--- project/ovp/frontend/ovp/components/manage/service/repository.vue
+++ project/ovp/frontend/ovp/components/manage/service/repository.vue
@@ -2,15 +2,30 @@
   <div class="px-4">
     <div class="v-group gap-2">
       <div class="font-semibold text-neutral-700">설명</div>
-      <div class="editable-group">
-        <span class="editable-group-desc">
-          서비스 A에 대한 설명이 기재됩니다.
-        </span>
-        <button class="button button-neutral-ghost button-sm" type="button">
-          <span class="hidden-text">수정</span>
-          <svg-icon class="svg-icon" name="pen"></svg-icon>
-        </button>
-      </div>
+      <editable-group
+        compKey="repositoryDescription"
+        :editable="true"
+        @editCancel="editCancel"
+        @editDone="editDone"
+        @editIcon="editIconClick"
+      >
+        <template #edit-slot>
+          <label class="hidden-text" for="textarea-modify">설명 입력</label>
+          <textarea
+            class="textarea"
+            v-model="newData.description"
+            @input="editInput($event)"
+            placeholder="설명에 대한 영역입니다."
+            required
+            id="textarea-modify"
+          ></textarea>
+        </template>
+        <template #view-slot>
+          <span class="editable-group-desc">
+            {{ newData.description }}
+          </span>
+        </template>
+      </editable-group>
     </div>
     <table class="mt-4">
       <colgroup>
@@ -41,7 +56,7 @@
       </tr>
     </table>
     <!-- 결과 없을 시 no-result 표시 -->
-    <div class="no-result max-h-40" style="display: none">
+    <div class="no-result" style="display: none">
       <div class="notification">
         <svg-icon class="notification-icon" name="info"></svg-icon>
         <p class="notification-detail">데이터 리스트가 없습니다.</p>
@@ -49,4 +64,85 @@
     </div>
   </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import EditableGroup from "@extends/editable-group/EditableGroup.vue";
+import { useServiceCollectionLogStore } from "@/store/admin/service/collection-log/index";
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+import _ from "lodash";
+
+const serviceCollectionLogStore = useServiceCollectionLogStore();
+const { updateRepositoryDescriptionAPI } = serviceCollectionLogStore;
+const { serviceData } = storeToRefs(serviceCollectionLogStore);
+
+interface RepositoryDescription {
+  description: string;
+}
+
+// 초기 데이터(description) 백업
+let defaultData: RepositoryDescription = {
+  description: "",
+};
+
+// 수정될 새로운 데이터
+const newData = computed(() => {
+  return _.cloneDeep(serviceData.value); // 값이 조회되거나 수정이 되면 newData로 값이 주입.
+});
+
+// JSON Patch 형식으로 데이터 변환 함수
+interface JsonPatchOperation {
+  op: string;
+  path: string;
+  value: any;
+}
+
+const createJsonPatch = (oldData: any, newData: any): JsonPatchOperation[] => {
+  const patch: JsonPatchOperation[] = [];
+  if (oldData.description !== newData.description) {
+    patch.push({
+      op: "replace",
+      path: "/description",
+      value: newData.description,
+    });
+  }
+  return patch;
+};
+
+// 사용자 입력 처리
+const editInput = (event: Event) => {
+  const target = event.target as HTMLTextAreaElement;
+  console.log(`event: ${target.value}`);
+};
+
+// 수정 버튼 클릭 시 호출
+const editIconClick = () => {
+  defaultData = _.cloneDeep(newData.value);
+};
+
+// 취소 버튼 클릭 시 호출
+const editCancel = () => {
+  serviceData.value = _.cloneDeep(defaultData);
+};
+
+// 수정 완료 버튼 클릭 시 호출
+const editDone = async () => {
+  const patchData = createJsonPatch(defaultData, newData.value);
+
+  try {
+    const response = await updateRepositoryDescriptionAPI(patchData);
+    if (response.result === 1) {
+      serviceData.value.description = response.description;
+      alert("수정이 완료되었습니다.");
+    } else {
+      serviceData.value.description = defaultData.description;
+      alert("수정에 실패하였습니다.");
+    }
+  } catch (error) {
+    console.error("API 호출 중 오류 발생: ", error);
+    serviceData.value.description = defaultData.description;
+  }
+};
+</script>
+
+<style scoped></style>
