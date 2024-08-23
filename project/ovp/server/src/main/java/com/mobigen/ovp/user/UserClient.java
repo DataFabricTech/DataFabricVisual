739827eb96dc10695544bb51acac179eb355b01f
ygkim97 <dnjdn21@naver.com>
2024-08-23 11:50:41 +0900

    [FAB2023-1194] feat: 사용자 추가 모달 관련 API 구현 - 랜덤 비밀번호 발급 API - 사용자 이름 중복 체크 API - 사용자 정보 추가 API

--- project/ovp/server/src/main/java/com/mobigen/ovp/user/UserClient.java
+++ project/ovp/server/src/main/java/com/mobigen/ovp/user/UserClient.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,4 +30,13 @@ public interface UserClient {
     ResponseEntity<Void> deleteUser(@PathVariable UUID id,
                                     @RequestParam(defaultValue = "true") Boolean recursive,
                                     @RequestParam(defaultValue = "false") Boolean hardDelete);
+
+    @GetMapping("/users/generateRandomPwd")
+    String getRandomPwd() throws Exception;
+
+    @GetMapping("/users/name/{name}")
+    Map<String, Object> checkDuplicateName(@PathVariable String name) throws Exception;
+
+    @PostMapping("/users")
+    Map<String, Object> addUser(@RequestBody Map<String, Object> param) throws Exception;
 }
--- project/ovp/server/src/main/java/com/mobigen/ovp/user/UserController.java
+++ project/ovp/server/src/main/java/com/mobigen/ovp/user/UserController.java
@@ -7,10 +7,13 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -66,5 +69,41 @@ public class UserController {
     @DeleteMapping("/{id}")
     public Object deleteUser(@PathVariable UUID id) throws Exception {
         return userService.deleteUser(id);
+    }
+
+    /**
+     * 관리자 > 랜덤 비밀번호 발급
+     *
+     * @return
+     * @throws Exception
+     */
+    @ResponseJsonResult(errorMessage = "랜덤 비밀번호 자동 생성 오류")
+    @GetMapping("/random-pwd")
+    public Object getRandomPwd() throws Exception {
+        return userService.getRandomPwd();
+    }
+
+    /**
+     * 관리자 > 사용자 이름 중복 체크
+     *
+     * @return
+     * @throws Exception
+     */
+    @ResponseJsonResult(errorMessage = "사용자 이름 중복 체크 오류")
+    @GetMapping("/check-name/{name}")
+    public Object checkDuplicateName(@PathVariable String name) throws Exception {
+        return userService.checkDuplicateName(name);
+    }
+
+    /**
+     * 관리자 > 사용자 정보 추가
+     *
+     * @return
+     * @throws Exception
+     */
+    @ResponseJsonResult(errorMessage = "사용자 정보 추가 오류")
+    @PostMapping("/add")
+    public Object addUser(@RequestBody Map<String, Object> params) throws Exception {
+        return userService.addUser(params);
     }
 }
