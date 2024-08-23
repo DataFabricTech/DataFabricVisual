550e049b3ba49634a90fe95818df199acbfc1fd7
soraJang <skysora85@gmail.com>
2024-08-23 11:31:52 +0900

    [FAB2023-1033] fix : upload 원복
    - 서비스 추가 에서 해당 기능 사용하지 않음

--- project/ovp/frontend/common/components/base/upload/Upload.vue
+++ project/ovp/frontend/common/components/base/upload/Upload.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="upload">
     <div class="upload-file">
-      <input
-        type="file"
-        class="upload-file-input"
-        v-bind:accept="inputAcceptList || undefined"
-        @change="handleFileChange"
-      />
+      <input type="file" class="upload-file-input">
       <button class="button button-neutral-stroke button-lg" :disabled="isDisabled">
         <svg-icon class="button-icon" name="upload"></svg-icon>
-        <span class="button-title">{{ props.uploadBtnText }}</span>
+        <span class="button-title">File Upload</span>
       </button>
     </div>
     <div v-if="isLoadingVisible" class="upload-status">
       <Loading class="loader-xs" :use-hidden-text="true"></Loading>
     </div>
-    <div v-if="isLoadedVisible && selectedFile !== null" class="upload-status">
-      <p class="upload-file-name">{{ selectedFile?.name ?? "" }}</p>
-      <button class="button link-button button-sm" @click="deleteUpload">
+    <div v-if="isLoadedVisible" class="upload-status">
+      <p class="upload-file-name">파일명.cst</p>
+      <button class="button link-button button-sm">
         <span class="hidden-text">닫기</span>
         <svg-icon class="button-icon" name="close"></svg-icon>
       </button>
@@ -25,8 +20,8 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineEmits, defineProps } from "vue";
+<script setup>
+import { ref, defineComponent } from 'vue';
 
 const props = defineProps({
   isLoadingVisible: {
@@ -40,50 +35,8 @@ const props = defineProps({
   isDisabled: {
     type: Boolean,
     default: false
-  },
-  uploadBtnText: {
-    type: String,
-    default: "File Upload"
-  },
-  allowedExtensions: {
-    type: Array,
-    default: () => []
-  },
-  useInputAccept: {
-    type: Boolean,
-    default: false
   }
 });
-
-const inputAcceptList = ref<string | null>(null);
-if (props.useInputAccept) {
-  inputAcceptList.value = props.allowedExtensions.join(",");
-}
-
-const emit = defineEmits<{
-  (e: "changed", file: any): void;
-}>();
-
-const selectedFile = ref<File | null>(null);
-const handleFileChange = ($event: Event) => {
-  const input = $event.target as HTMLInputElement;
-
-  if (input.files && input.files.length > 0) {
-    const file = input.files[0];
-    const fileExtension = file.name.split(".").pop()?.toLowerCase();
-
-    if (fileExtension && props.allowedExtensions.includes(`.${fileExtension}`)) {
-      selectedFile.value = file;
-      emit("changed", selectedFile.value);
-    } else {
-      alert("Invalid file type. Only .crt, .pem, .key files are allowed.");
-    }
-  }
-};
-
-const deleteUpload = () => {
-  selectedFile.value = null;
-};
 </script>
 
 <style lang="scss" scoped>
