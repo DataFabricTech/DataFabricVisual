2a7d6f7374e5dae1e8e79b13e896e24a8f3e8f7c
ohmy04180 <ohmy0418@mobigen.com>
2024-08-26 17:01:43 +0900

    [FAB2023-998] refactor: 최근 탐색 데이터 삭제, 추천 많은 데이터

--- project/ovp/frontend/ovp/pages/index.vue
+++ project/ovp/frontend/ovp/pages/index.vue
@@ -4,54 +4,7 @@
       <Loading :use-loader-overlay="true" class="loader-lg"></Loading>
     </div>
     <div class="l-split">
-      <div class="main-content">
-        <div class="l-top-bar">
-          <span class="main-content-title">최근 탐색 데이터</span>
-        </div>
-        <div class="no-result" v-if="isRecentQuestDataNoInfo">
-          <div class="notification">
-            <svg-icon class="notification-icon" name="info"></svg-icon>
-            <p class="notification-detail">출력할 정보가 없습니다.</p>
-          </div>
-        </div>
-        <!--        TODO: [개발] 최근 탐색 데이터 API 확인 후 추가 예정 -->
-        <resource-box-list
-          v-else
-          :use-prv-btn="false"
-          :data-list="recentQuestData"
-          :use-list-checkbox="false"
-          :show-owner="true"
-          :show-category="true"
-          :is-box-selected-style="false"
-          @modelNmClick="modelNmClick"
-        />
-      </div>
-      <div class="main-content">
-        <div class="l-top-bar">
-          <span class="main-content-title">북마크 한 데이터</span>
-          <nuxt-link :to="'/portal/my-page'" class="button link-button-support">
-            <span class="button-title">모두 보기</span>
-          </nuxt-link>
-        </div>
-        <div class="no-result" v-if="isBookmarkDataNoInfo">
-          <div class="notification">
-            <svg-icon class="notification-icon" name="info"></svg-icon>
-            <p class="notification-detail">출력할 정보가 없습니다.</p>
-          </div>
-        </div>
-        <resource-box-list
-          v-else
-          :use-prv-btn="false"
-          :data-list="bookmarkData"
-          :use-list-checkbox="false"
-          :show-owner="true"
-          :show-category="true"
-          :is-box-selected-style="false"
-          @modelNmClick="modelNmClick"
-        />
-      </div>
-    </div>
-    <div class="l-split">
+      <!--      TODO: [퍼블리싱] 6개로 보이도록 수정 필요 -->
       <div class="main-content">
         <div class="l-top-bar">
           <span class="main-content-title">추천 많은 데이터</span>
@@ -72,6 +25,32 @@
           v-else
           :use-prv-btn="false"
           :data-list="upVotesData"
+          :use-list-checkbox="false"
+          :show-owner="true"
+          :show-category="true"
+          :is-box-selected-style="false"
+          @modelNmClick="modelNmClick"
+        />
+      </div>
+    </div>
+    <div class="l-split">
+      <div class="main-content">
+        <div class="l-top-bar">
+          <span class="main-content-title">북마크 한 데이터</span>
+          <nuxt-link :to="'/portal/my-page'" class="button link-button-support">
+            <span class="button-title">모두 보기</span>
+          </nuxt-link>
+        </div>
+        <div class="no-result" v-if="isBookmarkDataNoInfo">
+          <div class="notification">
+            <svg-icon class="notification-icon" name="info"></svg-icon>
+            <p class="notification-detail">출력할 정보가 없습니다.</p>
+          </div>
+        </div>
+        <resource-box-list
+          v-else
+          :use-prv-btn="false"
+          :data-list="bookmarkData"
           :use-list-checkbox="false"
           :show-owner="true"
           :show-category="true"
@@ -127,14 +106,11 @@ const { setSortFilter } = searchCommonStore;
 const { currentTab } = storeToRefs(searchCommonStore);
 
 const mainCommonStore = useMainStore();
-const { getRecentQuestData, getUserInfo, getUpVotesData, getLastUpdatedData } =
-  mainCommonStore;
+const { getUserInfo, getUpVotesData, getLastUpdatedData } = mainCommonStore;
 const {
-  recentQuestData,
   bookmarkData,
   upVotesData,
   lastUpdatedData,
-  isRecentQuestDataNoInfo,
   isBookmarkDataNoInfo,
   isUpVotesDataNoInfo,
   isLastUpdatedData,
@@ -165,7 +141,6 @@ onMounted(() => {
   if (loader.value) {
     loader.value.style.display = "block";
   }
-  getRecentQuestData();
   getUserInfo();
   getLastUpdatedData();
   getUpVotesData();
