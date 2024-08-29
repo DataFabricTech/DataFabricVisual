7c2346b638532741cfc157bd102f5a4fad366291
ygkim97 <dnjdn21@naver.com>
2024-08-29 16:46:51 +0900

    fix: 메뉴 - 접근권한 체크하여 권한이 있는 메뉴만 조회 - 거버넌스와 관리페이지는 관리자만 접근가능

--- project/ovp/server/src/main/java/com/mobigen/ovp/menu/MenuService.java
+++ project/ovp/server/src/main/java/com/mobigen/ovp/menu/MenuService.java
@@ -2,6 +2,8 @@ package com.mobigen.ovp.menu;
 
 import com.mobigen.ovp.menu.entity.MenuEntity;
 import com.mobigen.ovp.menu.repository.MenuRepository;
+import com.mobigen.ovp.user.UserService;
+import com.mobigen.ovp.user.dto.UserInfoDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,15 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MenuService {
     private final MenuRepository menuRepository;
+    private final UserService userService;
 
-    public List<MenuEntity> getMenus() {
-        // ROOT 메뉴는 제외한다.
-        return menuRepository.findAllByUseYnTrueOrderByOrderAsc().stream()
-                .filter(menu -> !menu.getId().equals(menu.getParentId()))
+    public List<MenuEntity> getMenus() throws Exception {
+        // 메뉴 권한을 체크하여 조회한다.
+        UserInfoDTO user = (UserInfoDTO) userService.getUserInfo();
+        String menuAuthCode = user.isAdmin() ? "A" : "U";
+        
+        return menuRepository.findAllByUseYnTrueAndMenuAuthContainingOrderByOrderAsc(menuAuthCode).stream()
+                .filter(menu -> !menu.getId().equals(menu.getParentId())) // ROOT 메뉴는 제외한다.
                 .collect(Collectors.toList());
     }
 }
