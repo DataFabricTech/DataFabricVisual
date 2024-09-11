fd6e64761bf04bec5c8d562f6d268213eaef0a5a
leejeongkang <krj1226@gmail.com>
2024-09-11 15:18:50 +0900

    refactor: 용어 사전, 용어 정보 없을 시 처리

--- project/ovp/frontend/ovp/components/govern/glossary/glossary-info.vue
+++ project/ovp/frontend/ovp/components/govern/glossary/glossary-info.vue
@@ -60,90 +60,96 @@
         </div>
       </div>
       <!-- // 결과 없을 시 no-result 표시  -->
-
-      <div
-        class="editable-group"
-        v-if="Object.keys(glossary).length > 0 && !store.editGlossaryMode.des"
-      >
-        <span class="editable-group-desc">{{
-          glossary && glossary.description ? glossary.description : "설명 없음"
-        }}</span>
-        <button
-          class="button button-neutral-ghost button-sm"
-          type="button"
-          @click="changeEditGlossaryMode('des')"
+      <div class="v-group gap-2">
+        <div class="font-semibold text-neutral-700">설명</div>
+        <div
+          class="editable-group"
+          v-if="Object.keys(glossary).length > 0 && !store.editGlossaryMode.des"
         >
-          <span class="hidden-text">수정</span>
-          <svg-icon class="button-icon" name="pen"></svg-icon>
-        </button>
-      </div>
-      <!-- 수정 버튼 클릭시 아래 내용으로 전환됩니다 -->
-      <div class="editable-group" v-if="store.editGlossaryMode.des">
-        <label class="hidden-text" for="description-modify"
-          >카테고리 설명 수정
-        </label>
-        <textarea
-          id="description-modify"
-          class="textarea"
-          width="300px"
-          v-model="editData.description"
-        ></textarea>
-        <div class="h-group gap-1">
+          <span class="editable-group-desc">{{
+            glossary && glossary.description ? glossary.description : "-"
+          }}</span>
           <button
-            class="button button-neutral-stroke"
+            class="button button-neutral-ghost button-sm"
             type="button"
-            @click="cancel('des')"
+            @click="changeEditGlossaryMode('des')"
           >
-            취소
+            <span class="hidden-text">수정</span>
+            <svg-icon class="button-icon" name="pen"></svg-icon>
           </button>
-          <button
-            class="button button-primary-lighter"
-            type="button"
-            @click="
-              updateGlossary(glossary.id, {
-                op: 'replace',
-                path: '/description',
-                value: editData.description,
-              })
-            "
-          >
-            완료
-          </button>
+        </div>
+
+        <!-- 수정 버튼 클릭시 아래 내용으로 전환됩니다 -->
+        <div class="editable-group" v-if="store.editGlossaryMode.des">
+          <label class="hidden-text" for="description-modify"
+            >카테고리 설명 수정
+          </label>
+          <textarea
+            id="description-modify"
+            class="textarea"
+            width="300px"
+            v-model="editData.description"
+          ></textarea>
+          <div class="h-group gap-1">
+            <button
+              class="button button-neutral-stroke"
+              type="button"
+              @click="cancel('des')"
+            >
+              취소
+            </button>
+            <button
+              class="button button-primary-lighter"
+              type="button"
+              @click="
+                updateGlossary(glossary.id, {
+                  op: 'replace',
+                  path: '/description',
+                  value: editData.description,
+                })
+              "
+            >
+              완료
+            </button>
+          </div>
         </div>
       </div>
       <!-- // 수정 버튼 클릭시 아래 내용으로 전환됩니다 -->
-      <div class="editable-group" v-if="!store.editGlossaryMode.tag">
-        <div
-          class="tag tag-primary tag-sm"
-          v-if="glossary && glossary.tags && glossary.tags.length > 0"
-          v-for="tag in glossary.tags"
-        >
-          <span class="tag-text">{{ tag.displayName }}</span>
+      <div class="h-group gap-2">
+        <div class="font-semibold text-neutral-700 w-16">태그</div>
+        <div class="editable-group" v-if="!store.editGlossaryMode.tag">
+          <div
+            class="tag tag-primary tag-sm"
+            v-if="glossary && glossary.tags && glossary.tags.length > 0"
+            v-for="tag in glossary.tags"
+          >
+            <span class="tag-text">{{ tag.displayName }}</span>
+          </div>
+          <div class="text-neutral-700" v-else>
+            <span>-</span>
+          </div>
+          <button
+            class="button button-neutral-ghost button-sm"
+            type="button"
+            @click="changeEditGlossaryMode('tag')"
+          >
+            <span class="hidden-text">수정</span>
+            <svg-icon class="button-icon" name="pen"></svg-icon>
+          </button>
         </div>
-        <div class="text-neutral-700" v-else>
-          <span>태그 없음</span>
+        <div class="editable-group" v-if="store.editGlossaryMode.tag">
+          <menu-search-tag
+            :data="menuSearchTagsData"
+            :selected-items="glossary.tags"
+            label-key="displayName"
+            value-key="tagFQN"
+            :is-multi="true"
+            title="값을 선택하세요"
+            @multiple-change="changeTag"
+            @cancel="changeEditGlossaryMode('tag')"
+            @close="changeEditGlossaryMode('tag')"
+          ></menu-search-tag>
         </div>
-        <button
-          class="button button-neutral-ghost button-sm"
-          type="button"
-          @click="changeEditGlossaryMode('tag')"
-        >
-          <span class="hidden-text">수정</span>
-          <svg-icon class="button-icon" name="pen"></svg-icon>
-        </button>
-      </div>
-      <div class="editable-group" v-if="store.editGlossaryMode.tag">
-        <menu-search-tag
-          :data="menuSearchTagsData"
-          :selected-items="glossary.tags"
-          label-key="displayName"
-          value-key="tagFQN"
-          :is-multi="true"
-          title="값을 선택하세요"
-          @multiple-change="changeTag"
-          @cancel="changeEditGlossaryMode('tag')"
-          @close="changeEditGlossaryMode('tag')"
-        ></menu-search-tag>
       </div>
 
       <div>
