f023f5411440e2edff00d8f95ff2b392ff275935
jungseolhwa <dhqkfskrwl@naver.com>
2024-09-24 12:28:30 +0900

    [FAB2023-1244] refactor : work-list-contents 클래스 부여

--- project/ovp/frontend/ovp/pages/portal/govern/category/index.vue
+++ project/ovp/frontend/ovp/pages/portal/govern/category/index.vue
@@ -25,7 +25,7 @@
             <p class="notification-detail">등록된 정보가 없습니다.</p>
           </div>
         </div>
-        <div class="work-list-tree" v-else>
+        <div class="work-list-contents" v-else>
           <tree-vue
             v-if="categories && categories.length"
             mode="edit"
