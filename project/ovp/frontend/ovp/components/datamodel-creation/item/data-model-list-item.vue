ee8f9cf2cfe7ff3ea36c700742b5c0b0f20c0c24
kimsangwoo66 <mrk24565@naver.com>
2024-09-24 12:05:48 +0900

    [FAB2023-1213] fix: 데이터 모델 모달 추가 - 각 데이터 모델의 이미지가 나오도록 수정

--- project/ovp/frontend/ovp/components/datamodel-creation/item/data-model-list-item.vue
+++ project/ovp/frontend/ovp/components/datamodel-creation/item/data-model-list-item.vue
@@ -28,7 +28,7 @@
       @click="onClickItem"
       @contextmenu.prevent="onShowContextMenu"
     >
-      <div class="type-img type-img-oracle"></div>
+      <div class="type-img" :class="props.data.serviceIcon"></div>
       <span class="menu-text">{{ props.data.label }}</span>
       <span class="menu-subtext">{{ owner }}</span>
     </button>
