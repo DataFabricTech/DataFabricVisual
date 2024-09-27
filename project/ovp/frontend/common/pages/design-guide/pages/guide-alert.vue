53439196f2a7eefa04374c83abe8fbdae469e3eb
jungseolhwa <dhqkfskrwl@naver.com>
2024-09-27 17:20:19 +0900

    [FAB2023-1251] refactor : 얼럿 수정 사항 디자인 가이드 반영

--- project/ovp/frontend/common/pages/design-guide/pages/guide-alert.vue
+++ project/ovp/frontend/common/pages/design-guide/pages/guide-alert.vue
@@ -10,11 +10,10 @@
       <div class="flex flex-col">
         <div class="flex gap-8 mb-14">
           <figure class="section-figure items-center">
-            <div class="alert" style="width: 250px">
+            <div class="alert" style="width: 250px; position:static;transform: translate(0%, 0%);">
               <div class="alert-body">
-                <div class="notification">
-                  <svg-icon class="notification-icon" name="info"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
+                <div class="alert-text">
+                  얼럿 내용이 들어갑니다.
                 </div>
               </div>
               <div class="alert-foot">
@@ -29,102 +28,6 @@
             <h4 class="group-title">Normal</h4>
             <p class="group-summary">
               기본적인 상태 알림에 사용합니다.
-            </p>
-          </div>
-        </div>
-        <div class="flex gap-8 mb-14">
-          <figure class="section-figure items-center">
-            <div class="alert" style="width: 250px">
-              <div class="alert-body">
-                <div class="notification notification-info">
-                  <svg-icon class="notification-icon" name="info"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-              <div class="alert-foot">
-                <div class="alert-foot-group">
-                  <button class="button button-neutral-ghost button-lg">취소</button>
-                  <button class="button button-primary button-lg">확인</button>
-                </div>
-              </div>
-            </div>
-          </figure>
-          <div class="group-description">
-            <h4 class="group-title">Info</h4>
-            <p class="group-summary">
-              normal타입보다 중요한 정보를 알릴 때 사용됩니다.
-            </p>
-          </div>
-        </div>
-        <div class="flex gap-8 mb-14">
-          <figure class="section-figure items-center">
-            <div class="alert" style="width: 250px">
-              <div class="alert-body">
-                <div class="notification notification-warning">
-                  <svg-icon class="notification-icon" name="warning"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-              <div class="alert-foot">
-                <div class="alert-foot-group">
-                  <button class="button button-neutral-ghost button-lg">취소</button>
-                  <button class="button button-error button-lg">확인</button>
-                </div>
-              </div>
-            </div>
-          </figure>
-          <div class="group-description">
-            <h4 class="group-title">Warning</h4>
-            <p class="group-summary">
-              중요한 변화 또는 가벼운 경고를 나타낼 때 사용합니다.
-            </p>
-          </div>
-        </div>
-        <div class="flex gap-8 mb-14">
-          <figure class="section-figure items-center">
-            <div class="alert" style="width: 250px">
-              <div class="alert-body">
-                <div class="notification notification-error">
-                  <svg-icon class="notification-icon" name="error"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-              <div class="alert-foot">
-                <div class="alert-foot-group">
-                  <button class="button button-neutral-ghost button-lg">취소</button>
-                  <button class="button button-error button-lg">확인</button>
-                </div>
-              </div>
-            </div>
-          </figure>
-          <div class="group-description">
-            <h4 class="group-title">Error</h4>
-            <p class="group-summary">
-              계속 진행시 위험 또는 작업이 실패했을 때 사용합니다.
-            </p>
-          </div>
-        </div>
-        <div class="flex gap-8 mb-14">
-          <figure class="section-figure items-center">
-            <div class="alert" style="width: 250px">
-              <div class="alert-body">
-                <div class="notification notification-success">
-                  <svg-icon class="notification-icon" name="success"></svg-icon>
-                  <p class="notification-detail">얼럿 메세지를 입력해주세요.</p>
-                </div>
-              </div>
-              <div class="alert-foot">
-                <div class="alert-foot-group">
-                  <button class="button button-neutral-ghost button-lg">취소</button>
-                  <button class="button button-primary button-lg">확인</button>
-                </div>
-              </div>
-            </div>
-          </figure>
-          <div class="group-description">
-            <h4 class="group-title">Success</h4>
-            <p class="group-summary">
-              사용자의 작업 성공 알림에 사용합니다.
             </p>
           </div>
         </div>
