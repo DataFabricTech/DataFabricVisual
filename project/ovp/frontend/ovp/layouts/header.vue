e259b06a7851aa8cd85a9c8f8fb55c8fba935ed3
sominJoo <js06m13@gmail.com>
2025-01-09 10:00:01 +0900

    TTA 로고 변경

--- project/ovp/frontend/ovp/layouts/header.vue
+++ project/ovp/frontend/ovp/layouts/header.vue
@@ -3,8 +3,9 @@
     <div class="h-group">
       <h1 class="logo">
         <nuxt-link to="/" class="logo-link">
-          <svg-icon class="svg-icon logo-img" name="logo"></svg-icon>
-          <span class="hidden-text">logo</span>
+          <span class="text-neutral-700 text-17 font-bold"
+            >노후시설 재난안전 학습 데이터 관리</span
+          >
         </nuxt-link>
       </h1>
       <SearchInput
