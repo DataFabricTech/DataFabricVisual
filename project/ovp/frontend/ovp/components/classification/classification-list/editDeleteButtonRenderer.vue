39aaf8a8ce6a0e740184f35362c6b679a61b26e3
김지혜 <kimjihye6245@naver.com>
2025-04-07 14:02:53 +0900

    [FAB2023-1428] feat: 거버넌스 > 분류 > ag-grid로 목록 나오도록 교체

---
+++ project/ovp/frontend/ovp/components/classification/classification-list/editDeleteButtonRenderer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="data && data.name !== 'admin'" style="display: flex; gap: 8px;">
+  <button
+      v-if="data.name !== 'admin'"
+      class="button button-sm button-secondary-stroke"
+      type="button"
+      @click="editHandleClick"
+  >
+    <span class="button-title">수정</span>
+  </button>
+  <button
+      v-if="data.name !== 'admin'"
+      class="button button-sm button-error-stroke"
+      type="button"
+      @click="deleteHandleClick"
+  >
+    <span class="button-title">삭제</span>
+  </button>
+ </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  params: {
+    type: Object,
+  },
+});
+const data = props.params.data;
+
+const editHandleClick = () => {
+  props.params.context.tagModifyOpen(data);
+};
+const deleteHandleClick = () => {
+  props.params.context.confirmDelete(data);
+};
+</script>
