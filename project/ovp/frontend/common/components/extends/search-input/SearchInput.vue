5e25e6160ce4bcd6d2bb9cd1fefd3dc856140438
정설화 <tjfghk6133@hanmail.net>
2025-04-25 14:19:20 +0900

    [FAB2023-1421] feat: 오타 수정

--- project/ovp/frontend/common/components/extends/search-input/SearchInput.vue
+++ project/ovp/frontend/common/components/extends/search-input/SearchInput.vue
@@ -4,7 +4,7 @@
     <label class="hidden-text" :for="inpId">{{ labelText }}</label>
     <input
       :id="inpId"
-      class="text-input dd"
+      class="text-input"
       type="text"
       :value="inpValue"
       @keyup.enter="onClickSearch"
